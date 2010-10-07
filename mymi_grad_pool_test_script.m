aa26978a52d3aa9fd99420a5d985d04fd4c2006f
Luyi Chen <lychen1109@gmail.com>
2010-10-07 15:25:29 +0800

    init add mymi_grad_pool_test_script

---
+++ mymi_grad_pool_test_script.m
@@ -0,0 +1,9 @@
+matlabpool;
+
+tic;g=mymi_grad2(train_label,w,x,sigma);toc
+tic;g=mymi_grad2(train_label,w,x,sigma);toc
+tic;g=mymi_grad2(train_label,w,x,sigma);toc
+tic;g=mymi_grad2(train_label,w,x,sigma);toc
+
+matlabpool close
+
