5d69480b6d328f6a2d3524c8012db0b0a259cf5e
Luyi Chen <lychen1109@gmail.com>
2010-10-11 23:17:58 +0800

    remove unused files: mymi_grad parallel running time test

--- mymi_grad_pool_test_script.m
+++
@@ -1,9 +0,0 @@
-matlabpool;
-
-tic;g=mymi_grad2(train_label,w,x,sigma);toc
-tic;g=mymi_grad2(train_label,w,x,sigma);toc
-tic;g=mymi_grad2(train_label,w,x,sigma);toc
-tic;g=mymi_grad2(train_label,w,x,sigma);toc
-
-matlabpool close
-
