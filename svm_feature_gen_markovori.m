f7760420080609584f9d03a07da89f34ff189b27
Luyi Chen <lychen1109@gmail.com>
2010-10-12 16:22:41 +0800

    new variable idx

--- svm_feature_gen_markovori.m
+++ svm_feature_gen_markovori.m
@@ -1,13 +1,14 @@
 function f=svm_feature_gen_markovori(autrain)
 % extract feature with original markov feature
 
-T=uint16(3);
+T=3;
 transmat=extract_feature(autrain,1);
 f=zeros(size(transmat,3),(2*T+1)^2);
-
+idx=0;
 for i=-T:T
     for j=-T:T
         tmp=transmat(256+i,256+j,:);
-        f(:,(i-1)*(2*T+1)+j)=tmp(:);
+        idx=idx+1;
+        f(:,idx)=tmp(:);
     end
 end
