d35c46abcf873296632ef74b42c0bea5fa0a8b0d
Luyi Chen <lychen1109@gmail.com>
2010-10-12 15:50:21 +0800

    feature extract of original markov

---
+++ svm_feature_gen_markovori.m
@@ -0,0 +1,13 @@
+function f=svm_feature_gen_markovori(autrain)
+% extract feature with original markov feature
+
+T=3;
+transmat=extract_feature(autrain,1);
+f=zeros(size(transmat,3),(2*T+1)^2);
+
+for i=-T:T
+    for j=-T:T
+        tmp=transmat(256+i,256+j,:);
+        f(:,(i-1)*(2*T+1)+j)=tmp(:);
+    end
+end
