81ffca7dd699317622595ef7c2e1f6a40b7d7271
Luyi Chen <lychen1109@gmail.com>
2010-10-18 11:27:24 +0800

    test script for mymi3. check if it gives right result.

---
+++ mymi_test.m
@@ -0,0 +1,24 @@
+function mymi_test(label,x,sigma)
+%check if the new mymi function gives right result
+
+featuresize=size(x,2);
+numtry=4;
+
+for i=1:numtry
+    w=randn(1,featuresize);
+    w=w/norm(w);
+    disp('w = ');disp(w);
+    tic;I1=mymi2(label,x*w',sigma);t=toc;
+    fprintf('Old MI = %f\n',I1);
+    fprintf('Used time is %f second.\n',t);
+    tic;g1=mymi_grad2(label,w,x,sigma);t=toc;
+    fprintf('Old gradient is %f\n',g1);
+    fprintf('Used time is %f second.\n',t);
+    
+    tic;I2=mymi3(label,w,x,sigma);t=toc;
+    fprintf('new MI = %f\n',I2);
+    fprintf('used time %f seconds.\n',t);
+    tic;[I2,g2]=mymi3(label,w,x,sigma);t=toc;
+    fprintf('new MI=%f,  new gradient=%f\n',I2,g2);
+    fprintf('time used=%f\n',t);
+end
