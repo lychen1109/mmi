700d73dd41aa252fe4c089fee62b4e83a3d76db7
Luyi Chen <lychen1109@gmail.com>
2010-10-18 12:44:22 +0800

    fix output gradient format

--- mymi_test.m
+++ mymi_test.m
@@ -12,13 +12,16 @@ for i=1:numtry
     fprintf('Old MI = %f\n',I1);
     fprintf('Used time is %f second.\n',t);
     tic;g1=mymi_grad2(label,w,x,sigma);t=toc;
-    fprintf('Old gradient is %f\n',g1);
+    fprintf('Old gradient is\n');
+    disp(g1);
     fprintf('Used time is %f second.\n',t);
     
     tic;I2=mymi3(label,w,x,sigma);t=toc;
     fprintf('new MI = %f\n',I2);
     fprintf('used time %f seconds.\n',t);
     tic;[I2,g2]=mymi3(label,w,x,sigma);t=toc;
-    fprintf('new MI=%f,  new gradient=%f\n',I2,g2);
+    fprintf('new MI=%f\n',I2);
+    fprintf('new gradient=\n');
+    disp(g2);
     fprintf('time used=%f\n',t);
 end
