0f9f301596ba4e7bd36d4e522e0d4595c6cdda59
Luyi Chen <lychen1109@gmail.com>
2010-10-19 16:18:01 +0800

    add groupnum variable, add output dots

--- single_bin_grouping.m
+++ single_bin_grouping.m
@@ -1,4 +1,4 @@
-function group_single=single_bin_grouping(bin_mi,autrain,sptrain,label)
+function group_single=single_bin_grouping(bin_mi,autrain,sptrain,label,groupnum)
 %check the top 49 relevant bin in 9x9
 
 sigma=0.5;
@@ -11,7 +11,7 @@ group_single(centralarea(idx(1)))=1;
 [sx,sy]=ind2sub(size(bin_mi),centralarea(idx(1)));
 fprintf('group 1 selected at (%d,%d)\n',sx-256,sy-256);
 
-for i=2:30
+for i=2:groupnum
     unselected=find(group_single==-2);    
     jointmi=zeros(length(unselected),i-1);
     for k=1:length(unselected)
@@ -19,8 +19,10 @@ for i=2:30
             groupbins=find(group_single==j);
             features=group_feature_extract([groupbins' unselected(k)],autrain,sptrain);
             jointmi(k,j)=mymi3(label,eye(size(features,2)),features,sigma);
+            fprintf('.');
         end
     end
+    fprintf('\n');
     minjointmi=min(jointmi,[],2);
     [~,cidx]=max(minjointmi);
     group_single(unselected(cidx(1)))=i;
