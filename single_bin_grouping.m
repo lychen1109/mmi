0ec584f54e3fd2661bd0fd95e2c3b09f6b6e4507
Luyi Chen <lychen1109@gmail.com>
2010-10-19 11:11:55 +0800

    new single bin grouping using conditional MI

--- single_bin_grouping.m
+++ single_bin_grouping.m
@@ -1,6 +1,29 @@
-function group_single=single_bin_grouping(bin_mi)
-%check the top 49 relevant bin
+function group_single=single_bin_grouping(bin_mi,autrain,sptrain,label)
+%check the top 49 relevant bin in 9x9
 
+sigma=0.5;
 group_single=zeros(size(bin_mi));
-[~,I]=sort(bin_mi(:),1,'descend');
-group_single(I(1:49))=1:49;
+group_single(256+(-4:4),256+(-4:4))=-2;
+centralarea=find(group_single==-2);
+
+[~,idx]=sort(bin_mi(centralarea),1,'descend');
+group_single(centralarea(idx(1)))=1;
+[sx,sy]=ind2sub(size(bin_mi),centralarea(idx(1)));
+fprintf('group 1 selected at (%d,%d)\n',sx-256,sy-256);
+
+for i=2:30
+    unselected=find(group_single==-2);    
+    jointmi=zeros(length(unselected),i-1);
+    for k=1:length(unselected)
+        for j=1:i-1
+            groupbins=find(group_single==j);
+            features=group_feature_extract([groupbins' unselected(k)],autrain,sptrain);
+            jointmi(k,j)=mymi3(label,eye(size(features,2)),features,sigma);
+        end
+    end
+    minjointmi=min(jointmi,[],2);
+    [~,cidx]=max(minjointmi);
+    group_single(unselected(cidx(1)))=i;
+    [sx,sy]=ind2sub(size(bin_mi),unselected(cidx(1)));
+    fprintf('group %d selected on (%d,%d)\n',i,sx-256,sy-256);
+end
