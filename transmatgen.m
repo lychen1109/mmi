c683a072cd0bd50bfa9df937f87f1eeb00651dc9
Luyi Chen <lychen1109@gmail.com>
2010-10-20 15:02:10 +0800

    add bound to transmatgen

--- transmatgen.m
+++ transmatgen.m
@@ -1,12 +1,15 @@
-function fm=extract_feature(imgs,f)
+function fm=transmatgen(imgs,f,T)
 % extract fth transition matrix from imgs
+% f is direnction of transmat
+% T is the bound
 
 N=size(imgs,1);
-m_size=255*2+1;
+m_size=T*2+1;
 fm=zeros(m_size,m_size,N);
+origin=256;%origin poition
 for i=1:N
    img=imgs(i,:);
    img=reshape(img,128,128);
    D=tpm(img);
-   fm(:,:,i)=D(:,:,f);
+   fm(:,:,i)=D(origin+(-T:T),origin+(-T:T),f);
 end
