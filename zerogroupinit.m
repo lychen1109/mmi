70f73cfbc1591b7da0ed3984825a0ece8f6c9fc4
Luyi Chen <lychen1109@gmail.com>
2010-10-20 15:06:10 +0800

    calculate initial bin_mi with transition matrix

--- zerogroupinit.m
+++ zerogroupinit.m
@@ -1,11 +1,16 @@
-function bin_mi=zerogroupinit(group_idx,autrain,sptrain,label,sigma)
+function bin_mi=zerogroupinit(group_idx,label,sigma,transmat,bin_mi)
 %calculate MI of group 0
 
-bin_mi=zeros(size(group_idx));
 points=find(group_idx==0);
 NUMPOINTS=length(points);
+T=13;
 
 for i=1:NUMPOINTS
-    bin_feature=group_feature_extract(points(i),autrain,sptrain);
-    bin_mi(points(i))=mymi3(label,1,bin_feature,sigma);
+    [sx,sy]=ind2sub(size(group_idx),points(i));
+    feature=transmat(sx,sy,:);
+    if bin_mi(points(i))==0
+        I=mymi3(label,1,feature(:),sigma,0);
+        fprintf('MI at (%d,%d) = %f\n',sx-1-T,sy-1-T,I);
+    end
+    bin_mi(points(i))=I;
 end
