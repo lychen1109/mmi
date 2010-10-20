769682939c01ecc80a7c0a7857cc0a48d57f8472
Luyi Chen <lychen1109@gmail.com>
2010-10-20 16:12:23 +0800

    bug fix: non-zero bins are also updated

--- zerogroupinit.m
+++ zerogroupinit.m
@@ -5,12 +5,12 @@ points=find(group_idx==0);
 NUMPOINTS=length(points);
 T=13;
 
-for i=1:NUMPOINTS
-    [sx,sy]=ind2sub(size(group_idx),points(i));
-    feature=transmat(sx,sy,:);
+for i=1:NUMPOINTS    
     if bin_mi(points(i))==0
+        [sx,sy]=ind2sub(size(group_idx),points(i));
+        feature=transmat(sx,sy,:);
         I=mymi3(label,1,feature(:),sigma,0);
-        fprintf('MI at (%d,%d) = %f\n',sx-1-T,sy-1-T,I);
-    end
-    bin_mi(points(i))=I;
+        fprintf('MI at (%d,%d) = %e\n',sx-1-T,sy-1-T,I);
+        bin_mi(points(i))=I;
+    end    
 end
