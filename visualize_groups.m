531f3a16fa85a6e905eabb85f6ef8df203cf7a21
Luyi Chen <lychen1109@gmail.com>
2010-10-22 00:12:58 +0800

    remove boundary input

--- visualize_groups.m
+++ visualize_groups.m
@@ -1,9 +1,6 @@
-function visualize_groups(color_idx,map,B)
-%draw group points. B is the boundary 256-B:256+B
+function visualize_groups(color_idx,map)
+%draw group points. 
 
-origin=[256,256];
-
-color_idx=color_idx(origin(1)-B:origin(1)+B,origin(2)-B:origin(2)+B);
 color_idx(end+1,:)=0;
 color_idx(:,end+1)=0;
 
