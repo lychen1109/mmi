d338da58d4f0fb310101ee31f17e9ea1070a18ca
Luyi Chen <lychen1109@gmail.com>
2010-10-23 18:39:11 +0800

    change to calculate full initial weighting

--- weighting.m
+++ weighting.m
@@ -6,13 +6,13 @@ step=100;
 weight_idx=zeros(size(group_idx));
 MAXGROUP=max(group_idx(:));
 
-for grp=1:1 %MAXGROUP
+for grp=1:MAXGROUP
     if isempty(find(group_idx==grp,1))
         continue;
     end
     x=group_feature_extract(find(group_idx==grp),transmat);
     if size(x,2)>1            
-        [w,flag,iter]=gradient_descend(label,x,MAX_iter,step,1);
+        [w,flag,iter]=gradient_descend(label,x,MAX_iter,step,0);
         fprintf('w for group %d reached after %d iters with flag %d\n',grp,iter,flag);
     else
         w=1;
