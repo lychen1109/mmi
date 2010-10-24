5c75fae637c325d51173422547e3d0a3cb075d9d
Luyi Chen <lychen1109@gmail.com>
2010-10-24 17:57:16 +0800

    update weighting to use new gradient descend. add record output.

--- weighting.m
+++ weighting.m
@@ -1,10 +1,14 @@
-function weight_idx=weighting(group_idx,label,transmat)
+function [weight_idx,record]=weighting(group_idx,label,transmat,MAX_iter,step,Tol,display)
 %calculate the weighting of bins
 
-MAX_iter=1000;
-step=100;
+if isempty(MAX_iter), MAX_iter=1000; end
+if isempty(step),step=1; end
+if isempty(Tol), Tol=1e-5; end
+if isempty(display),display=0;end
+
 weight_idx=zeros(size(group_idx));
 MAXGROUP=max(group_idx(:));
+record=zeros(MAXGROUP,2);
 
 for grp=1:MAXGROUP
     if isempty(find(group_idx==grp,1))
@@ -12,8 +16,9 @@ for grp=1:MAXGROUP
     end
     x=group_feature_extract(find(group_idx==grp),transmat);
     if size(x,2)>1            
-        [w,flag,iter]=gradient_descend(label,x,MAX_iter,step,0);
+        [w,flag,iter]=gradient_descend(label,x,MAX_iter,step,display,Tol);
         fprintf('w for group %d reached after %d iters with flag %d\n',grp,iter,flag);
+        record(grp,:)=[iter flag];
     else
         w=1;
     end    
