9e78ebaa8f8f67d724da62fbf07be53ee0f1fbb8
Luyi Chen <lychen1109@gmail.com>
2010-10-24 16:56:31 +0800

    change tol to 1e-5. change step to 1

--- groupweight.m
+++ groupweight.m
@@ -2,8 +2,8 @@ function [w,flag,iter]=groupweight(grp,group_idx,transmat,label,Max_iter,step,To
 %calculate group weight
 
 if isempty(Max_iter), Max_iter=1000; end
-if isempty(step), step=100;end
-if isempty(Tol), Tol=1e-6;end
+if isempty(step), step=1;end
+if isempty(Tol), Tol=1e-5;end
 if isempty(display),display=0;end
 
 x=group_feature_extract(find(group_idx==grp),transmat);
