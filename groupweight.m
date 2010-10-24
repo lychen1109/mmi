bc634ece7eb2a4a25ee8e26623eb1740501fc174
Luyi Chen <lychen1109@gmail.com>
2010-10-24 13:49:29 +0800

    calculate one group's weight with gradient descend

---
+++ groupweight.m
@@ -0,0 +1,10 @@
+function [w,flag,iter]=groupweight(grp,group_idx,transmat,label,Max_iter,step,Tol,display)
+%calculate group weight
+
+if isempty(Max_iter), Max_iter=1000; end
+if isempty(step), step=100;end
+if isempty(Tol), Tol=1e-6;end
+if isempty(display),display=0;end
+
+x=group_feature_extract(find(group_idx==grp),transmat);
+[w,flag,iter]=gradient_descend(label,x,Max_iter,step,display,Tol);
