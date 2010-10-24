7f8b0cf43967919948d835f8f7a8d1fc4fb5323c
Luyi Chen <lychen1109@gmail.com>
2010-10-24 15:55:32 +0800

    groupmi selectively output lda mi

--- groupmi.m
+++ groupmi.m
@@ -1,12 +1,21 @@
-function mi=groupmi(group_idx,grp,w,label,transmat)
+function [mi,milda]=groupmi(group_idx,grp,w,label,transmat)
 %check group mi under w
 
 x=group_feature_extract(find(group_idx==grp),transmat);
 if size(x,2)>1
-    y=x*lda(label,x);
+    wlda=lda(label,x)';
+    y=x*wlda';
     sigma=(max(y)-min(y))/2;
 else
     sigma=(max(x)-min(x))/2;
 end
 
 mi=mymi3(label,w,x,sigma,0);
+
+if nargout>1
+    if size(x,2)>1
+        milda=mymi3(label,wlda,x,sigma,0);
+    else
+        milda=[];
+    end
+end
