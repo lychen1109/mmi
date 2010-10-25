445f1a2b3f073658e611e6d3aa8e372f3aa8317f
Luyi Chen <lychen1109@gmail.com>
2010-10-25 14:53:05 +0800

    update markov_original features gen to use transmat

--- svm_feature_gen_markovori.m
+++ svm_feature_gen_markovori.m
@@ -1,13 +1,12 @@
-function f=svm_feature_gen_markovori(autrain)
+function f=svm_feature_gen_markovori(transmat)
 % extract feature with original markov feature
 
 T=3;
-transmat=extract_feature(autrain,1);
 f=zeros(size(transmat,3),(2*T+1)^2);
 idx=0;
 for i=-T:T
     for j=-T:T
-        tmp=transmat(256+i,256+j,:);
+        tmp=transmat(14+i,14+j,:);
         idx=idx+1;
         f(:,idx)=tmp(:);
     end
