515afd5d104f2087e12d3fb223bc65bded917cb8
Luyi Chen <lychen1109@gmail.com>
2010-10-27 15:52:14 +0800

    change transmatgen to use tpm1

--- transmatgen.m
+++ transmatgen.m
@@ -1,15 +1,18 @@
-function fm=transmatgen(imgs,f,T)
+function fm=transmatgen(imgs,T)
 % extract fth transition matrix from imgs
-% f is direnction of transmat
 % T is the bound
 
 N=size(imgs,1);
-m_size=T*2+1;
+if T>0
+    m_size=T*2+1;
+else
+    m_size=2*255+1;
+end
 fm=zeros(m_size,m_size,N);
-origin=256;%origin poition
+
 for i=1:N
    img=imgs(i,:);
    img=reshape(img,128,128);
-   D=tpm(img);
-   fm(:,:,i)=D(origin+(-T:T),origin+(-T:T),f);
+   D=tpm1(img,T);
+   fm(:,:,i)=D;
 end
