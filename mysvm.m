a460282b8999d73796ab74d58d74858f1fba04ed
Luyi Chen <lychen1109@gmail.com>
2010-11-07 23:30:31 +0800

    change to use real param selection

--- mysvm.m
+++ mysvm.m
@@ -1,9 +1,7 @@
 function errnum=mysvm(dataTrain,grpTrain,dataTest,grpTest)
 %wrapper of svmtrain and svmpredict, return number of classification error
 
-%[c,g]=svmgrid(dataTrain,grpTrain);
-c=512;
-g=0.125;
+[c,g]=svmgrid(dataTrain,grpTrain);
 cmd=['-c ' num2str(c) ' -g ' num2str(g)];
 model=svmtrain(grpTrain,dataTrain,cmd);
 grpPred=svmpredict(grpTest,dataTest,model);
