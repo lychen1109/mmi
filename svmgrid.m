871f030f220aba471147537e24db50604d054837
Luyi Chen <lychen1109@gmail.com>
2010-11-07 19:58:04 +0800

    new input for range of c and g

--- svmgrid.m
+++ svmgrid.m
@@ -1,9 +1,14 @@
-function [bestc,bestg]=svmgrid(dataTrain,grpTrain)
+function [bestc,bestg]=svmgrid(dataTrain,grpTrain,rangec,rangeg)
 %parameter selection
 
+if nargin<4
+    rangec=-5:15;
+    rangeg=5:-1:-15;
+end
+
 bestcv=0;
-for log2c=-5:15
-    for log2g=4:-1:-15
+for log2c=rangec
+    for log2g=rangeg
         cmd=['-v 5 -c ' num2str(2^log2c) ' -g ' num2str(2^log2g)];
         cv=svmtrain(grpTrain,dataTrain,cmd);
         if cv>bestcv
