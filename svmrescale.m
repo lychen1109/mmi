e1a911aef031be16f9b3510591bbfab9850e4b08
Luyi Chen <lychen1109@gmail.com>
2010-11-10 14:12:23 +0800

    new file to rescale data for svm

---
+++ svmrescale.m
@@ -0,0 +1,4 @@
+function data_scale=svmrescale(data)
+%rescale data to [0 1]
+
+data_scale=(data - repmat(min(data,[],1),size(data,1),1)) * spdiags(1./(max(data,[],1)-min(data,[],1))',0,size(data,2),size(data,2));
