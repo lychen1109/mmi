2d270115499fdd02df8b8afc9a70952a13c3070f
Luyi Chen <lychen1109@gmail.com>
2010-11-19 10:38:34 +0800

    change featinc to double

--- RFE_search.m
+++ RFE_search.m
@@ -2,16 +2,16 @@ function history=RFE_search(label,data,history,featinc)
 %use RFE algotirhm to remove useless feature
 %featinc: features to include
 
-[bestc,bestg,bestcv]=svmgrid(data(:,featinc),label,0,2,-3:2:11,-11:2:1,500);
+[bestc,bestg,bestcv]=svmgrid(data(:,featinc==1),label,0,2,-3:2:11,-11:2:1,500);
 cv_tmp=history.cv;
 history.cv=[cv_tmp;bestcv];
 
 cmd=['-c ' num2str(bestc) ' -g ' num2str(bestg)];
-model=svmtrain(label,data(:,featinc),cmd);
+model=svmtrain(label,data(:,featinc==1),cmd);
 
 delta=zeros(size(featinc));
 delta_W2=RFE_evaluate(model);
-delta(featinc)=delta_W2(:);
+delta(featinc==1)=delta_W2(:);
 delta_tmp=history.delta;
 history.delta=[delta_tmp;delta];
 
