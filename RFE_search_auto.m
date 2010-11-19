94a8fa99d22eb9ee860d631a10ef3e83c169f7d0
Luyi Chen <lychen1109@gmail.com>
2010-11-19 16:36:25 +0800

    auto remove features

---
+++ RFE_search_auto.m
@@ -0,0 +1,26 @@
+function history=RFE_search_auto(label,data,history,featinc)
+%use RFE algotirhm to remove useless feature
+%featinc: features to include
+
+while sum(featinc)>1
+    [bestc,bestg,bestcv]=svmgrid(data(:,featinc==1),label,0,2,-3:2:9,-11:2:1,500);
+    cv_tmp=history.cv;
+    history.cv=[cv_tmp;bestcv];
+    
+    cmd=['-c ' num2str(bestc) ' -g ' num2str(bestg)];
+    model=svmtrain(label,data(:,featinc==1),cmd);
+    
+    delta=zeros(size(featinc));
+    delta_W2=RFE_evaluate(model);
+    delta(featinc==1)=delta_W2(:);
+    delta_tmp=history.delta;
+    history.delta=[delta_tmp;delta];
+    
+    feat_tmp=history.feat;
+    history.feat=[feat_tmp;featinc];
+    
+    minidx=min_delta(delta);
+    featinc(minidx)=0;
+end
+
+
