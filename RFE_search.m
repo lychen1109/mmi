4e7ce7767e1cb4cd4556847531b468a00323e74f
Luyi Chen <lychen1109@gmail.com>
2010-11-19 10:17:55 +0800

    add input parameter history

--- RFE_search.m
+++ RFE_search.m
@@ -1,9 +1,20 @@
-function history=RFE_search(label,data)
-%use RFE algotirhm to remove feature one by one
+function history=RFE_search(label,data,history,featinc)
+%use RFE algotirhm to remove useless feature
+%featinc: features to include
 
-[bestc,bestg,bestcv]=svmgrid(data,label,0);
-history.cv=bestcv;
+[bestc,bestg,bestcv]=svmgrid(data(:,featinc),label,0,2,-3:2:11,-11:2:1,500);
+cv_tmp=history.cv;
+history.cv=[cv_tmp;bestcv];
+
 cmd=['-c ' num2str(bestc) ' -g ' num2str(bestg)];
-model=svmtrain(label,data,cmd);
+model=svmtrain(label,data(:,featinc),cmd);
+
+delta=zeros(size(featinc));
 delta_W2=RFE_evaluate(model);
-history.delta=delta_W2;
+delta(featinc==1)=delta_W2(:);
+delta_tmp=history.delta;
+history.delta=[delta_tmp;delta];
+
+feat_tmp=history.feat;
+history.feat=[feat_tmp;featinc];
+
