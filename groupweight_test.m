b3eefb49b2207df8c9563fbc559967e3f8309d80
Luyi Chen <lychen1109@gmail.com>
2010-11-22 11:10:30 +0800

    auto test every group

--- groupweight_test.m
+++ groupweight_test.m
@@ -1,1 +1,8 @@
-tic;[Ipre,Gpre]=groupweight(1,8,weight_idx,obs_scale(holdoutCVP.training,:),label(holdoutCVP.training,:));toc
+function groupweight_test(weight_idx,feat_idx,label,data)
+%test function of groupweight
+
+n_group=size(weight_idx,2);
+for i=1:n_group
+    groupweight(i,feat_idx,weight_idx,data,label,1000,1,1e-6,1);
+end
+
