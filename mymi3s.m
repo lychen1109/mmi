f74f9acda609711f9861c015292c84f09b13efb3
Luyi Chen <lychen1109@gmail.com>
2010-11-24 15:52:35 +0800

    rewrite mymi3 sample version

--- mymi3s.m
+++ mymi3s.m
@@ -2,19 +2,48 @@ function [I,G]=mymi3s(label,w,x,pairs,sigma)
 %sample version of mymi3
 
 n_pairs=size(pairs,1);
+N=size(label,1);
+J0=sum(label==0);
+J1=N-J0;
+J=(J0^2+J1^2)/N^2;
 y=x*w;
 I=0;
 G=0;
-GK0=gauss_kernel(0,sigma);
-for i=1:n_pairs
-    i1=pairs(i,1);
-    i2=pairs(i,2);
-    if label(i1)==label(i2)
-        continue;
+
+parfor i=1:n_pairs
+    sample_pair=pairs(i,:);
+    i1=sample_pair(1);
+    i2=sample_pair(2);
+    
+    ly=y;
+    lx=x;
+    llabel=label;
+    D=(ly(i1,:)-ly(i2,:))';
+    GK=gauss_kernel(D,sigma);
+    if llabel(i1)==llabel(i2)
+        Vin=GK;
+        Fin=GK*D*lx(i2,:)/sigma^2;
+    else
+        Vin=0;
+        Fin=0;
     end
-    D=(y(i1,:)-y(i2,:))';
-    GKD=gauss_kernel(D,sigma);
-    I=I+0.25*(GK0-GKD);
-    G=G-(0.125/sigma^2)*GKD*D*(x(i2,:)-x(i1,:));
+    Vall=J*GK;
+    Fall=J*GK*D*lx(i2,:)/sigma^2;
+    
+    if llabel(i1)==0
+        Jp=J0;
+    else
+        Jp=J1;
+    end
+    if llabel(i2)==0
+        Jc=J0;
+    else
+        Jc=J1;
+    end
+    Vbtw=Jp*GK/N;
+    Fbtw=(Jp+Jc)*GK*D*lx(i2,:)/(2*N*sigma^2);
+    
+    I=I+Vin+Vall-2*Vbtw;
+    G=G+Fin+Fall-2*Fbtw;    
 end
 G=G';
