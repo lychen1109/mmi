94eb7a895b34de1689144f811bc433b487ebaccf
Luyi Chen <lychen1109@gmail.com>
2010-11-27 17:17:56 +0800

    change to use function handal

--- transmatgen.m
+++ transmatgen.m
@@ -1,8 +1,10 @@
-function fm=transmatgen(imgs,T,type)
+function fm=transmatgen(imgs,T,type,f)
 % extract transition matrix from imgs
 % T is the bound
 %type=0: with elements outside of T
 %type=1: without elements outside of T
+%f calculates the transition probility
+%D=f(img,T);
 
 N=size(imgs,1);
 if type==0
@@ -15,7 +17,7 @@ fm=zeros(m_size,m_size,N);
 for i=1:N
    img=imgs(i,:);
    img=reshape(img,128,128);
-   D=tpm1(img,T);
+   D=f(img,T);
    if type>0
        D=D(2:end-1,2:end-1);
    end
