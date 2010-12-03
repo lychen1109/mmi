c4325468133206d2b0bcb6c973078f146c7c9373
Luyi Chen <lychen1109@gmail.com>
2010-12-03 14:30:13 +0800

    rewrite bin accuracy calculation

--- ldaaccu.m
+++ ldaaccu.m
@@ -1,11 +1,15 @@
-function bin_ldaaccu=ldaaccu(group_idx,label,transmat)
-%calculate lda accuracy of each selected bin
+function bin_ldaaccu=ldaaccu(label,feat,holdoutCVP)
+%calculate lda accuracy of each bin
 
-bin_ldaaccu=zeros(size(group_idx));
-bins=find(group_idx==0);
+n_feat=size(feat,2);
+bin_ldaaccu=zeros(n_feat,1);
 
-for i=1:length(bins)
-    x=group_feature_extract(bins(i),transmat);
-    [~,err]=classify(x,x,label);
-    bin_ldaaccu(bins(i))=1-err;
+for i=1:n_feat
+    datatrain=feat(holdoutCVP.training,i);
+    datatest=feat(holdoutCVP.test,i);
+    labeltrain=label(holdoutCVP.training);
+    labeltest=label(holdoutCVP.test);
+    predict=classify(datatest,datatrain,labeltrain);
+    n_test=size(labeltest,1);
+    bin_ldaaccu(i)=sum(predict==labeltest)/n_test;
 end
