887788b3933adba32f531235c733b6c72d8e2f6b
Luyi Chen <lychen1109@gmail.com>
2010-12-08 16:51:21 +0800

    remove rescale part

--- markovfeat.m
+++ markovfeat.m
@@ -1,18 +1,18 @@
-function f=markovfeat(transmat,isnormal)
+function f=markovfeat(transmat)
 % extract feature with original markov feature
 
 n_sample=size(transmat,3);
 range=size(transmat,1);
-if isnormal>0
-    for i=1:n_sample
-        tm=transmat(:,:,i);
-        for j=1:range
-            if sum(tm(j,:))>0
-                tm(j,:)=tm(j,:)/sum(tm(j,:));
-            end
+
+for i=1:n_sample
+    tm=transmat(:,:,i);
+    for j=1:range
+        if sum(tm(j,:))>0
+            tm(j,:)=tm(j,:)/sum(tm(j,:));
         end
-        transmat(:,:,i)=tm;
     end
+    transmat(:,:,i)=tm;
 end
+
 f=reshape(transmat,range^2,n_sample)';
-f=svmrescale(f);
+
