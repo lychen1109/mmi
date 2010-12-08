6a9b154bb0af557755dbaf75e63fc7a99ce6d827
Luyi Chen <lychen1109@gmail.com>
2010-12-09 00:10:52 +0800

    output Z

--- svmgrid_np.m
+++ svmgrid_np.m
@@ -1,4 +1,4 @@
-function [bestc,bestg,bestcv]=svmgrid_np(dataTrain,grpTrain,display,type,rangec,rangeg,mem)
+function [bestc,bestg,bestcv,Z]=svmgrid_np(dataTrain,grpTrain,display,type,rangec,rangeg,mem)
 %parameter selection, nonparallel version
 
 if nargin<4
@@ -11,24 +11,31 @@ if nargin<5
 end
 
 bestcv=0;
+Z=zeros(length(rangeg),length(rangec));
+i=0;
+j=0;
 
 for log2g=rangeg
+    i=i+1;
     for log2c=rangec
+        j=j+1;
         if type==0
             cmd=['-v 5 -c ' num2str(2^log2c) ' -t 0 -m ' num2str(mem)];
         else
             cmd=['-v 5 -c ' num2str(2^log2c) ' -g ' num2str(2^log2g) ' -m ' num2str(mem)];
         end
         cv=svmtrain(grpTrain,dataTrain,cmd);
+        Z(i,j)=cv;
         if bestcv<cv
             bestcv=cv;
             bestlog2c=log2c;
             bestlog2g=log2g;
         end
         if display>0
-            fprintf('%g %g %g\n',log2c,log2g,cv);
+            fprintf('%g %g %g\n',log2g,log2c,cv);
         end
     end
+    j=0;
 end
 
 bestc=2^bestlog2c;
