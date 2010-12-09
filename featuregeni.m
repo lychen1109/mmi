72779d5f9c5c460a9231cd40ff5f383583f27bf1
Luyi Chen <lychen1109@gmail.com>
2010-12-09 15:57:28 +0800

    add channel input to featuregeni

--- featuregeni.m
+++ featuregeni.m
@@ -1,10 +1,10 @@
-function [features,features_cr]=featuregeni(path,T,fun)
+function features=featuregeni(path,T,channel,fun)
 %pixel space feature generation
 
 files=dir([path filesep '*.tif']);
 n_files=length(files);
 features=zeros(2*T+1,2*T+1,n_files);
-features_cr=zeros(2*T+1,2*T+1,n_files);
+
 for i=1:n_files
     img=imread([path filesep files(i).name]);
     if isa(img,'uint16')
@@ -12,10 +12,14 @@ for i=1:n_files
     else
         img=double(img);
     end
-    [img_y,~,img_cr]=myrgb2ycbcr(img);    
-    D=fun(img_y,T);
+    [img_y,img_cb,img_cr]=myrgb2ycbcr(img);
+    if channel==1
+        img_ch=img_y;
+    elseif channel==2
+        img_ch=img_cb;
+    else
+        img_ch=img_cr;
+    end
+    D=fun(img_ch,T);
     features(:,:,i)=D;    
-        
-    D_cr=fun(img_cr,T);
-    features_cr(:,:,i)=D_cr;
 end
