a5683506c9c5443cfceec81ea21549f22c2b6fae
Luyi Chen <lychen1109@gmail.com>
2010-12-09 14:15:37 +0800

    add cr output

--- featuregeni.m
+++ featuregeni.m
@@ -1,9 +1,10 @@
-function features=featuregeni(path,T,fun)
+function [features,features_cr]=featuregeni(path,T,fun)
 %pixel space feature generation
 
 files=dir([path filesep '*.tif']);
 n_files=length(files);
 features=zeros(2*T+1,2*T+1,n_files);
+features_cr=zeros(2*T+1,2*T+1,n_files);
 for i=1:n_files
     img=imread([path filesep files(i).name]);
     if isa(img,'uint16')
@@ -11,8 +12,13 @@ for i=1:n_files
     else
         img=double(img);
     end
-    img=0.3*img(:,:,1)+0.59*img(:,:,2)+0.11*img(:,:,3);
-    img=round(img);
-    D=fun(img,T);
+    img_y=0.3*img(:,:,1)+0.59*img(:,:,2)+0.11*img(:,:,3);
+    img_y=round(img_y);
+    D=fun(img_y,T);
     features(:,:,i)=D;
+    
+    img_cr=128+0.5*img(:,:,1)-0.419*img(:,:,2)-0.0813*img(:,:,3);
+    img_cr=round(img_cr);
+    D_cr=fun(img_cr,T);
+    features_cr(:,:,i)=D_cr;
 end
