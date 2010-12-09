fdec9661bbc7951fbc69538bf36fb44613c563f0
Luyi Chen <lychen1109@gmail.com>
2010-12-09 15:12:50 +0800

    add cr output

--- featuregen.m
+++ featuregen.m
@@ -1,14 +1,16 @@
-function features=featuregen(path,T,fun)
+function [features,features_cr]=featuregen(path,T,fun)
 %generate features from path
 
 files=dir([path filesep '*.tif']);
 n_files=length(files);
 features=zeros(2*T+1,2*T+1,n_files);
+features_cr=zeros(2*T+1,2*T+1,n_files);
 for i=1:n_files
     img=imread([path filesep files(i).name]);
     if isa(img,'uint16')
         img=uint8(round(double(img)*255/65535));
-    end    
+    end
+    [~,~,img_cr]=myrgb2ycbcr(img);
     t='temp.jpg';
     imwrite(img,t,'jpg','Quality',100);
     clear img;
@@ -17,6 +19,10 @@ for i=1:n_files
     img=abs(jobj.coef_arrays{1});
     D=fun(img,T);
     features(:,:,i)=D;
+    
+    bdct_cr=abs(round(blkproc(img_cr,[8 8],@dct2)));
+    D_cr=fun(bdct_cr,T);
+    features_cr(:,:,i)=D_cr;
 end
 
 
