4cd92dffbfcdeaed5f3d2ed463d6ce9bcefcdde2
Luyi Chen <lychen1109@gmail.com>
2011-01-04 12:17:59 +0800

    updated to support precomputed kernel

--- svmtest.m
+++ svmtest.m
@@ -1,15 +1,23 @@
-function [m,s]=svmtest(class,data,svmstr,k,n_test)
+function [m,s]=svmtest(class,data,c,g,type,k,n_test,kernel)
 %test the svm accuracy with k outer loop
 
 ac=zeros(k,1);
+svmstr=['-c ' num2str(c) ' -g ' num2str(g) ' -t ' num2str(type)];
 for i=1:k    
     CVP=cvpartition(class,'holdout',n_test);
     dataTrain=data(CVP.training,:);
-    grpTrain=class(CVP.training);    
-    model=svmtrain(grpTrain,dataTrain,svmstr);    
+    n_train=size(dataTrain,1);
+    grpTrain=class(CVP.training);
     dataTest=data(CVP.test,:);
     grpTest=class(CVP.test);
-    predict=svmpredict(grpTest,dataTest,model);    
+    
+    if type==4
+        model=svmtrain(grpTrain,[(1:n_train)' dataTrain*kernel*dataTrain'],svmstr);
+        predict=svmpredict(grpTest,[(1:n_test)' dataTest*kernel*dataTrain'],model);
+    else
+        model=svmtrain(grpTrain,dataTrain,svmstr);
+        predict=svmpredict(grpTest,dataTest,model);
+    end
     ac(i)=sum(predict==grpTest)/n_test;
     disp(ac(i));
 end
