fa406e82be107d4694209f6846bd7b16e132f309
Luyi Chen <lychen1109@gmail.com>
2011-01-06 13:53:44 +0800

    fix output param of function call

--- svmgrid2.m
+++ svmgrid2.m
@@ -4,7 +4,7 @@ function svmgrid2(grp,data,display,type,rangec,rangeg,kernel,T,n_test)
 
 for t=1:T
     traincvp=cvpartition(grp,'holdout',n_test);
-    [c,g,cv]=svmgrid_np(grp(traincvp.training),data(traincvp.training,:),display,type,rangec,rangeg,kernel);    
+    [g,c,cv]=svmgrid_np(grp(traincvp.training),data(traincvp.training,:),display,type,rangec,rangeg,kernel);    
     fprintf('%g %g %g\n',g,c,cv);
 end
 
