887b5ea7efadbb74a457193884efc1880eac848c
Luyi Chen <lychen1109@gmail.com>
2011-01-13 10:22:03 +0800

    remove unused file

--- sumcmatana.m
+++
@@ -1,9 +0,0 @@
-function sumcmatana(sumcmat)
-%printf percentage of points in according center area
-
-ALL=128*126*1441;
-for T=3:50
-    center=sumcmat(256+(-T:T),256+(-T:T));
-    num=sum(center(:));
-    fprintf('T=%d, percent=%f\n',T,num/ALL);
-end
