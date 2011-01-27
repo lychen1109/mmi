d50235638a14fbc07e290baf914b295f338fd8ce
Luyi Chen <lychen1109@gmail.com>
2011-01-27 14:45:51 +0800

    calculate one kernel matrix pertime

--- klkernel.m
+++ klkernel.m
@@ -1,21 +1,13 @@
-function [K_train,K_test]=klkernel(datatrain,datatest,g,type)
+function K=klkernel(set1,set2,g,type)
 %return training and test kernel matrix
 
-n_train=size(datatrain,1);
-n_test=size(datatest,1);
-K_train=zeros(n_train,n_train);
-K_test=zeros(n_test,n_train);
-
-for i=1:n_train
-    for j=1:n_train
-        kld=kl_div(datatrain(i,:),datatrain(j,:),type);
-        K_train(i,j)=exp(-g*kld);
+n1=size(set1,1);
+n2=size(set2,1);
+K=zeros(n1,n2);
+for i=1:n1
+    for j=1:n2
+        kld=kl_div(set1(i,:),set2(j,:),type);
+        K(i,j)=exp(-g*kld);
     end
 end
 
-for i=1:n_test
-    for j=1:n_train
-        kld=kl_div(datatest(i,:),datatrain(j,:),type);
-        K_test(i,j)=exp(-g*kld);
-    end
-end
