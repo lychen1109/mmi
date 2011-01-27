1e3df66a092ea123c4ebb34d3ee62e57a867f9c9
Luyi Chen <lychen1109@gmail.com>
2011-01-27 16:35:51 +0800

    remove Z and kernel input

--- svmgrid_np.m
+++ svmgrid_np.m
@@ -1,37 +1,24 @@
-function [bestg,bestc,bestcv,Z]=svmgrid_np(grpTrain,dataTrain,display,type,rangec,rangeg,kernel)
+function [bestg,bestc,bestcv]=svmgrid_np(grpTrain,dataTrain,display,rangec,rangeg)
 %parameter selection, nonparallel version
 
 bestcv=0;
-Z=zeros(length(rangeg),length(rangec));
-i=0;
-j=0;
-[n_data,~]=size(dataTrain);
 more_cmd=' -e 0.1 -h 1';
 
-for log2g=rangeg
-    i=i+1;
-    for log2c=rangec
-        j=j+1;        
-        cmd=['-v 5 -c ' num2str(2^log2c) ' -g ' num2str(2^log2g) ' -t ' num2str(type) more_cmd];
-        if type==4
-            cv=svmtrain(grpTrain,[(1:n_data)' dataTrain*kernel*dataTrain'],cmd);
-        else
-            cv=svmtrain(grpTrain,dataTrain,cmd);
-        end
-        Z(i,j)=cv;
+for log2g=rangeg    
+    for log2c=rangec                
+        cmd=['-v 5 -c ' num2str(2^log2c) ' -g ' num2str(2^log2g) more_cmd];        
+        cv=svmtrain(grpTrain,dataTrain,cmd);                
         if bestcv<cv
             bestcv=cv;
-            bestlog2c=log2c;
-            bestlog2g=log2g;
+            bestc=2^log2c;
+            bestg=2^log2g;
         end
         if display>0
             fprintf('%g %g %g\n',log2g,log2c,cv);
         end
-    end
-    j=0;
+    end    
 end
 
-bestc=2^bestlog2c;
-bestg=2^bestlog2g;
+
 
 
