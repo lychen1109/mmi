6b6248af79a387421e7354b69a80bed80504a854
Luyi Chen <lychen1109@gmail.com>
2011-01-27 13:49:39 +0800

    introduce gamma

--- klkernel.m
+++ klkernel.m
@@ -1,4 +1,4 @@
-function [K_train,K_test]=klkernel(datatrain,datatest,type)
+function [K_train,K_test]=klkernel(datatrain,datatest,g,type)
 %return training and test kernel matrix
 
 n_train=size(datatrain,1);
@@ -9,13 +9,13 @@ K_test=zeros(n_test,n_train);
 for i=1:n_train
     for j=1:n_train
         kld=kl_div(datatrain(i,:),datatrain(j,:),type);
-        K_train(i,j)=exp(-kld);
+        K_train(i,j)=exp(-g*kld);
     end
 end
 
 for i=1:n_test
     for j=1:n_train
         kld=kl_div(datatest(i,:),datatrain(j,:),type);
-        K_test(i,j)=exp(-kld);
+        K_test(i,j)=exp(-g*kld);
     end
 end
