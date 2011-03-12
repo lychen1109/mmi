c924379ed0337e4e0190936b7d2b1b4f386fe9dd
Luyi Chen <lychen1109@gmail.com>
2011-03-12 17:57:54 +0800

    add nSV output

--- svmtest.m
+++ svmtest.m
@@ -1,7 +1,8 @@
-function [m,s]=svmtest(class,data,c,g,type,k,n_test,kernel)
+function [m,s,mnSV]=svmtest(class,data,c,g,type,k,n_test,kernel)
 %test the svm accuracy with k outer loop
 
 ac=zeros(k,1);
+nSV=zeros(k,1);
 svmstr=['-c ' num2str(c) ' -g ' num2str(g) ' -t ' num2str(type)];
 for i=1:k    
     CVP=cvpartition(class,'holdout',n_test);
@@ -13,15 +14,17 @@ for i=1:k
     
     if type==4
         model=svmtrain(grpTrain,[(1:n_train)' dataTrain*kernel*dataTrain'],svmstr);
-        predict=svmpredict(grpTest,[(1:n_test)' dataTest*kernel*dataTrain'],model);
+        [~,accu,~]=svmpredict(grpTest,[(1:n_test)' dataTest*kernel*dataTrain'],model);
     else
         model=svmtrain(grpTrain,dataTrain,svmstr);
-        predict=svmpredict(grpTest,dataTest,model);
+        [~,accu,~]=svmpredict(grpTest,dataTest,model);
     end
-    ac(i)=sum(predict==grpTest)/n_test;
-    disp(ac(i));
+    nSV(i)=model.totalSV;
+    ac(i)=accu(1);
+    fprintf('accuracy %g with nSV %d\n',accu(1),model.totalSV);
 end
 m=mean(ac);
 s=std(ac);
+mnSV=mean(nSV);
 
     
