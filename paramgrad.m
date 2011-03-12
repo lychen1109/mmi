abbc58fcc287d82b4b3f51095b7a23e070f2e4d9
Luyi Chen <lychen1109@gmail.com>
2011-03-12 16:05:14 +0800

    fix typo in outputvp

--- paramgrad.m
+++ paramgrad.m
@@ -147,8 +147,8 @@ grad(4)=sum(LpB1)+sum(LpB2);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %calc objective function
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Like1=log(1./(1+exp(A*outputp+B)));
-Like2=log(1-1./(1+exp(A*outputn+B)));
+Like1=log(1./(1+exp(A*outputvp+B)));
+Like2=log(1-1./(1+exp(A*outputvn+B)));
 Like=sum(Like1)+sum(Like2);
 
 
