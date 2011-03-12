22f536b60dfcb34e63c03fe71fa126a78dbaec4c
Luyi Chen <lychen1109@gmail.com>
2011-03-12 15:05:29 +0800

    add cross validation

--- paramlearn.m
+++ paramlearn.m
@@ -2,11 +2,22 @@ function theta=paramlearn(labeltrain,datatrain)
 %paramlearn: learn the best parameter of standard gauss kernel and
 %probability params
 
-validationcvp=cvpartition(labeltrain,'holdout',400);
+K=3; %fold number
+cvp=cvpartition(labeltrain,'Kfold',K);
 theta=[0 0 -1 0]; %initial value
 cmd=['-c ' num2str(2^theta(1)) ' -g ' num2str(2^theta(2))];
-model=svmtrain(labeltrain(validationcvp.training),datatrain(validationcvp.training,:),cmd);
-[~,~,dvalues]=svmpredict(labeltrain(validationcvp.test),datatrain(validationcvp.test,:),model);
-grad=paramgrad(labeltrain(validationcvp.test),datatrain(validationcvp.test,:),dvalues,model,theta);
-disp('grad is');
-disp(grad);
+accus=zeros(K,1);
+grad=zeros(K,size(theta,2));
+for i=1:K
+    fprintf('processing fold:%d\n',i);
+    model=svmtrain(labeltrain(cvp.training(i)),datatrain(cvp.training(i),:),cmd);
+    [~,accu,dvalues]=svmpredict(labeltrain(cvp.test(i)),datatrain(cvp.test(i),:),model);
+    fprintf('accuracy:%g\n',accu(1));
+    accus(i)=accu(1);
+    grad(i,:)=paramgrad(labeltrain(cvp.test(i)),datatrain(cvp.test(i),:),dvalues,model,theta);
+    disp('grad is');
+    disp(grad(i,:));
+end
+fprintf('average accuracy: %g\n',mean(accus));
+fprintf('full grad is\n');
+disp(sum(grad));
