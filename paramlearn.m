9853dd7cd60bd175ca1dbb30709ce78125e09065
Luyi Chen <lychen1109@gmail.com>
2011-03-12 16:23:50 +0800

    add loop and update theta in every loop

--- paramlearn.m
+++ paramlearn.m
@@ -6,22 +6,29 @@ K=3; %fold number
 cvp=cvpartition(labeltrain,'Kfold',K);
 theta=[0 0 -1 0]; %initial value
 cmd=['-c ' num2str(2^theta(1)) ' -g ' num2str(2^theta(2))];
+step=0.01;
 
-accus=zeros(K,1);
-grad=zeros(K,size(theta,2));
-Like=zeros(K,1);
-for i=1:K
-    fprintf('processing fold:%d\n',i);
-    model=svmtrain(labeltrain(cvp.training(i)),datatrain(cvp.training(i),:),cmd);
-    [~,accu,dvalues]=svmpredict(labeltrain(cvp.test(i)),datatrain(cvp.test(i),:),model);
-    fprintf('accuracy:%g\n',accu(1));
-    accus(i)=accu(1);
-    [grad(i,:),Like(i)]=paramgrad(labeltrain(cvp.test(i)),datatrain(cvp.test(i),:),dvalues,model,theta);
-    disp('grad is');
-    disp(grad(i,:));
-    fprintf('objective fun is %g\n',Like(i));    
+for loop=1:10
+    accus=zeros(K,1);
+    grad=zeros(K,size(theta,2));
+    Like=zeros(K,1);
+    for i=1:K
+        fprintf('processing fold:%d\n',i);
+        model=svmtrain(labeltrain(cvp.training(i)),datatrain(cvp.training(i),:),cmd);
+        [~,accu,dvalues]=svmpredict(labeltrain(cvp.test(i)),datatrain(cvp.test(i),:),model);
+        fprintf('accuracy:%g\n',accu(1));
+        accus(i)=accu(1);
+        [grad(i,:),Like(i)]=paramgrad(labeltrain(cvp.test(i)),datatrain(cvp.test(i),:),dvalues,model,theta);
+        disp('grad is');
+        disp(grad(i,:));
+        fprintf('objective fun is %g\n',Like(i));
+    end
+    disp('++++++++++++++++++++');
+    fprintf('average accuracy: %g\n',mean(accus));
+    fullgrad=sum(grad);
+    fprintf('full grad is\n');
+    disp(fullgrad);
+    fprintf('full objective fun is %g\n',sum(Like));
+    disp('++++++++++++++++++++');
+    theta=theta+step*fullgrad;
 end
-fprintf('average accuracy: %g\n',mean(accus));
-fprintf('full grad is\n');
-disp(sum(grad));
-fprintf('full objective fun is %g\n',sum(Like));
