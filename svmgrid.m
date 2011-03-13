ab326eb399b5e5a24a99d25cc498ce0dd33da827
Luyi Chen <lychen1109@gmail.com>
2011-03-13 08:40:19 +0800

    use real value of C and g in svmgrid

--- svmgrid.m
+++ svmgrid.m
@@ -7,9 +7,9 @@ cv=zeros(nc,ng);
         
 for i=1:nc
     cvrow=zeros(1,ng);
-    log2c=rangec(i);
+    c=rangec(i);
     parfor j=1:ng        
-        cmd=['-v 5 -c ' num2str(2^log2c) ' -g ' num2str(2^rangeg(j)) ' -t ' num2str(type)];        
+        cmd=['-v 5 -c ' num2str(c) ' -g ' num2str(rangeg(j)) ' -t ' num2str(type)];        
         cvrow(j)=svmtrain(grpTrain,dataTrain,cmd);               
     end
     cv(i,:)=cvrow;
@@ -18,7 +18,7 @@ end
 [bestcv,I]=max(cv(:));
 [indc,indg]=ind2sub(size(cv),I);
 
-bestc=2^rangec(indc);
-bestg=2^rangeg(indg);
+bestc=rangec(indc);
+bestg=rangeg(indg);
 
 
