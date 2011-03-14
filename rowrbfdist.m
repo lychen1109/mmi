b4e83c6a72c6eabbaa41083433f788ebc8955198
Luyi Chen <lychen1109@gmail.com>
2011-03-14 17:09:24 +0800

    use parfor in rowrbfdist

--- rowrbfdist.m
+++ rowrbfdist.m
@@ -2,7 +2,7 @@ function d=rowrbfdist(x,y,theta)
 %row rbf kernel distance weighted by kernel params
 
 d=0;
-for i=1:9
+parfor i=1:9
     t=false(9,9);
     t(i,:)=true(1,9);
     d=d+2^theta(i+1)*norm(x(t)-y(t))^2;
