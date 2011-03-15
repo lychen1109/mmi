c9ede2de1b1064c3d0d6738322f32023af0af11c
Luyi Chen <lychen1109@gmail.com>
2011-03-15 10:33:03 +0800

    new file for kernel compare

---
+++ kernelcompare.m
@@ -0,0 +1,13 @@
+function kernelcompare(x,y)
+%compare distance and computing time of two kernels
+
+tic;
+dist1=exp(-2^0*norm(x-y)^2);
+t=toc;
+fprintf('gauss distance is %g, calculated in %d sec\n',dist1,t);
+
+theta=zeros(1,10);
+tic;
+dist2=rowrbfdist(x,y,theta);
+t=toc;
+fprintf('row rbf distance is %g, calculated in %d sec\n',dist2,t);
