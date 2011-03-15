3781b433b238752500c846d55d04e3f339408f1c
Luyi Chen <lychen1109@gmail.com>
2011-03-15 15:50:29 +0800

    change rowsvmfun to calculate Ktest only on SVs

--- rowsvmfun.m
+++ rowsvmfun.m
@@ -19,12 +19,21 @@ Ktrain=exp(-Ktrain);
 t=toc;
 fprintf('kernel of training calculated in %d sec\n',t);
 
+cmd=['-c ' num2str(2^theta(1)) ' -t 4'];
+model=svmtrain(labeltrain,[(1:n_train)' Ktrain],cmd);
+
+SVs=full(model.SVs);
+svidx=zeros(1,n_train);
+svidx(SVs)=1;
+
 tic;
 for i=1:n_test
     datatesti=datatest(i,:);
     Ktestrow=zeros(1,n_train);
     parfor j=1:n_train
-        Ktestrow(j)=rowrbfdist(datatesti,datatrain(j,:),theta);
+        if svidx(j)==1
+            Ktestrow(j)=rowrbfdist(datatesti,datatrain(j,:),theta);
+        end
     end
     Ktest(i,:)=Ktestrow;
 end
@@ -32,8 +41,6 @@ Ktest=exp(-Ktest);
 t=toc;
 fprintf('kernel of test calculated in %d sec\n',t);
 
-cmd=['-c ' num2str(2^theta(1)) ' -t 4'];
-model=svmtrain(labeltrain,[(1:n_train)' Ktrain],cmd);
 [~,accu,dvalues]=svmpredict(labeltest,[(1:n_test)' Ktest],model);
 
 
