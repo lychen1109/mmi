52878f32bad06cbc48046b6344141240ca79e022
Luyi Chen <lychen1109@gmail.com>
2011-03-20 13:03:16 +0800

    new way for pre-allocating object array

--- dataspliter.m
+++ dataspliter.m
@@ -1,7 +1,7 @@
 function cvparray=dataspliter(N,label,n_test)
 %randomly generate split sets
 
-cvparray=repmat(cvpartition(label,'holdout',n_test),N,1);
+cvparray(1:N)=cvpartition(label,'holdout',n_test);
 for i=2:N
     cvparray(i)=cvpartition(label,'holdout',n_test);
 end
