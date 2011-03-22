4dcf9c56f989689d968d6c05cde563e859de12fa
Luyi Chen <lychen1109@gmail.com>
2011-03-22 15:00:50 +0800

    add kernel computing time output in fullsvmfun

--- fullsvmfun.m
+++ fullsvmfun.m
@@ -7,12 +7,15 @@ Ktrain=zeros(n_train,n_train);
 Ktest=zeros(n_test,n_train);
 C=2^theta(1);
 
+tic;
 for i=1:n_train
     for j=1:n_train
         Ktrain(i,j)=fullrbfdist(datatrain(i,:),datatrain(j,:),theta);
     end
 end
 Ktrain=exp(-Ktrain);
+t=toc;
+fprintf('training kernel calculated in %g sec\n',t);
 
 cmd=['-c ' num2str(C) ' -t 4'];
 model=svmtrain(labeltrain,[(1:n_train)' Ktrain],cmd);
@@ -23,6 +26,7 @@ nSV=model.totalSV;
 svidx=full(model.SVs);
 Ktest1=zeros(n_test,nSV);
 
+tic;
 for i=1:n_test
     for j=1:nSV
         Ktest1(i,j)=fullrbfdist(datatest(i,:),datasv(j,:),theta);
@@ -30,4 +34,7 @@ for i=1:n_test
 end
 Ktest(:,svidx)=Ktest1;
 Ktest=exp(-Ktest);
+t=toc;
+fprintf('test kernel calculated in %g sec\n',t);
+
 [~,accu,dvalues]=svmpredict(labeltest,[(1:n_test)' Ktest],model);
