ada2b1260913f923cef4c944346900cf06394df7
Luyi Chen <lychen1109@gmail.com>
2011-03-25 15:50:27 +0800

    add de check option

--- paramlearnotb.m
+++ paramlearnotb.m
@@ -3,7 +3,7 @@ function [theta,output]=paramlearnotb(labeltrain,datatrain,theta,mysvmfun,paramg
 
 K=5; %fold number
 cvp=cvpartition(labeltrain,'Kfold',K);
-opt=optimset('GradObj','on','LargeScale','off','display','iter-detailed');
+opt=optimset('GradObj','on','LargeScale','off','display','iter-detailed','de','on');
 [theta,fval,exitflag,output]=fminunc(@myfun,theta,opt);
 fprintf('optimization finished with fval=%g, and exitflag %d\n',fval,exitflag);
 
