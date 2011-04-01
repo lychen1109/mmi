018ca56e7cb4e5834d0065c730581e07a61b2304
Luyi Chen <lychen1109@gmail.com>
2011-04-01 13:17:25 +0800

    init checkin testrun

---
+++ testrun.m
@@ -0,0 +1,4 @@
+%script to run test
+%running test with standard rbf kernel, direct method for A and B, smoothed
+%error as objective fun
+[theta,output]=paramlearnotb2(label(cvp.training),feat(cvp.training,:),[0 0],@mysvmfun,@paramgrad,@logistregdirect,@smootherror,@svmoutputgraderr);
