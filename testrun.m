17a661f40e15890f2c03e6b5b62edd757e119616
Luyi Chen <lychen1109@gmail.com>
2011-04-01 14:36:01 +0800

    change opt in paramlearnotb2

--- testrun.m
+++ testrun.m
@@ -1,5 +1,5 @@
 %script to run test
 %running test with standard rbf kernel, direct method for A and B, smoothed
 %error as objective fun
-%[theta,output]=paramlearnotb2(label(cvp.training),feat(cvp.training,:),[0 0],@mysvmfun,@paramgrad,@logistregdirect,@smootherror,@svmoutputgraderr);
-decheck(label(cvp.training),feat(cvp.training,:),[0 0],@mysvmfun,@paramgrad,@logistregdirect,@smootherror,@svmoutputgraderr);
+[theta,output]=paramlearnotb2(label(cvp.training),feat(cvp.training,:),[0 0],@mysvmfun,@paramgrad,@logistregdirect,@smootherror,@svmoutputgraderr);
+%decheck(label(cvp.training),feat(cvp.training,:),[0 0],@mysvmfun,@paramgrad,@logistregdirect,@smootherror,@svmoutputgraderr);
