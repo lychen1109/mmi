1d39ffb7e8b4e0f05e8ea445bc8559d295711fb0
Luyi Chen <lychen1109@gmail.com>
2011-04-06 10:13:31 +0800

    modify testrun: test gradient with 1/5 of samples

--- testrun.m
+++ testrun.m
@@ -1,5 +1,6 @@
 %script to run test
 %running test with standard rbf kernel, direct method for A and B, smoothed
 %error as objective fun
-[theta,output]=paramlearnotb2(label(cvp.training),feat(cvp.training,:),[0 0],@mysvmfun,@paramgrad,@logistregdirect,@smootherror,@svmoutputgraderr);
+%[theta,output]=paramlearnotb2(label(cvp.training),feat(cvp.training,:),[0 0],@mysvmfun,@paramgrad,@logistregdirect,@smootherror,@svmoutputgraderr);
 %decheck(label(cvp.training),feat(cvp.training,:),[0 0],@mysvmfun,@paramgrad,@logistregdirect,@smootherror,@svmoutputgraderr);
+[theta,output]=paramlearnotb2(label(cvp.test(1)),feat(cvp.test(1),:),[0 0],@mysvmfun,@paramgrad,@logistreg,@svmllhood,@svmoutputgrad);
