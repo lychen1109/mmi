cc7dea0f7e21dc871a4d0add5660bc4f55b13c83
Luyi Chen <lychen1109@gmail.com>
2011-04-06 10:14:11 +0800

    output -L in objfun

--- svmllhood.m
+++ svmllhood.m
@@ -5,4 +5,4 @@ outputvp=outputv(labelv==1);
 outputvn=outputv(labelv==0);
 Like1=log(1./(1+exp(A*outputvp+B)));
 Like2=log(1-1./(1+exp(A*outputvn+B)));
-L=sum(Like1)+sum(Like2);
+L=-sum(Like1)-sum(Like2);
