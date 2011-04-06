348bd39eae03627169aa381b2514e513528ddcfc
Luyi Chen <lychen1109@gmail.com>
2011-04-06 10:38:58 +0800

    fix sign bug in logistreg

--- logistreg.m
+++ logistreg.m
@@ -8,11 +8,10 @@ A=x(1);
 B=x(2);
 
     function [L,g]=myfun(x)
-        L=-svmllhood(labelv,dvalues,x(1),x(2));
+        L=svmllhood(labelv,dvalues,x(1),x(2));
         if nargout>1
             g=zeros(1,2);
-            [g(1),g(2)]=svmlogistgrad(labelv,dvalues,x(1),x(2));
-            g=-g;
+            [g(1),g(2)]=svmlogistgrad(labelv,dvalues,x(1),x(2));            
         end        
     end
 end
--- svmlogistgrad.m
+++ svmlogistgrad.m
@@ -7,5 +7,5 @@ LpA1=-outputvp.*exp(A*outputvp+B)./(1+exp(A*outputvp+B)); %gradient of L with A 
 LpA2=outputvn.*exp(-A*outputvn-B)./(1+exp(-A*outputvn-B)); %gradient of L with A of negative samples
 LpB1=-exp(A*outputvp+B)./(1+exp(A*outputvp+B));
 LpB2=exp(-A*outputvn-B)./(1+exp(-A*outputvn-B));
-pa=sum(LpA1)+sum(LpA2);
-pb=sum(LpB1)+sum(LpB2);
+pa=-sum(LpA1)-sum(LpA2);
+pb=-sum(LpB1)-sum(LpB2);
