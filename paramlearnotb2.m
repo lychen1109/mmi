6e13aae14dbed69a7b1e57235ad39040b2e91ede
Luyi Chen <lychen1109@gmail.com>
2011-04-07 10:41:56 +0800

    initialize dvalues and modelstruct in nested fun; print average validation accuracy

--- paramlearnotb2.m
+++ paramlearnotb2.m
@@ -2,21 +2,23 @@ function [theta,fval,exitflag,output]=paramlearnotb2(labeltrain,datatrain,theta,
 %paramlearn toolbox version using fminunc
 %move A and B out of theta
 
-modelstructs(1:5)=emptymodelstruct;
 n_data=size(datatrain,1);
-dvalues=zeros(n_data,1);
 K=5; %fold number
 cvp=cvpartition(labeltrain,'Kfold',K);
 opt=optimset('GradObj','on','LargeScale','off','display','iter-detailed','DerivativeCheck','on','diffmin',1e-2,'Tolfun',1e-3,'tolx',1e-3);
 [theta,fval,exitflag,output]=fminunc(@myfun,theta,opt);
-fprintf('optimization finished with fval=%g, and exitflag %d\n',fval,exitflag);
 
     function [L,grad]=myfun(theta)
         fprintf('evaluating myfun with theta\n');
-        disp(theta);        
+        disp(theta);
+        modelstructs(1:K)=emptymodelstruct;
+        dvalues=zeros(n_data,1);
+        ac=zeros(K,1);
         for i=1:K
-            [modelstructs(i),~,dvalues(cvp.test(i))]=mysvmfun(labeltrain(cvp.training(i)),datatrain(cvp.training(i),:),labeltrain(cvp.test(i)),datatrain(cvp.test(i),:),theta);
+            [modelstructs(i),accu,dvalues(cvp.test(i))]=mysvmfun(labeltrain(cvp.training(i)),datatrain(cvp.training(i),:),labeltrain(cvp.test(i)),datatrain(cvp.test(i),:),theta);
+            ac(i)=accu(1);
         end
+        fprintf('average validation accuracy is %g (%g)\n',mean(ac),std(ac));
         [A,B]=logistreg(labeltrain,dvalues);
         L=objfun(labeltrain,dvalues,A,B);
         fprintf('logistic regression result: A=%g, B=%g, L=%g\n',A,B,L);
