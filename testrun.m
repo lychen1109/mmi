2180270103a3931463bbd848d8af3f81c87acc97
Luyi Chen <lychen1109@gmail.com>
2011-04-07 16:44:08 +0800

    update testrun

--- testrun.m
+++ testrun.m
@@ -8,4 +8,7 @@
 %[theta,output]=paramlearnotb2(label(cvp.training),feat(cvp.training,:),[0 0],@mysvmfun,@paramgrad,@logistreg,@svmllhood,@svmoutputgrad);
 
 %row gamma, max likelihood
-[theta,fval,exitflag,output]=paramlearnotb2(label(cvp.training),feat(cvp.training,:),zeros(1,10),@rowsvmfun,@rowparamgrad,@logistreg,@svmllhood,@svmoutputgrad);
+%[theta,fval,exitflag,output]=paramlearnotb2(label(cvp.training),feat(cvp.training,:),zeros(1,10),@rowsvmfun,@rowparamgrad,@logistreg,@svmllhood,@svmoutputgrad);
+
+%batch learn of row gamma for cvpa
+[thetaa_row1,fvala,exitflaga,outputc1]=rowgammalearn(label,feat,cvpa,thetaa_init_zero);
