ce6ced869e103340cb81684fb05c5df5a12ff184
Luyi Chen <lychen1109@gmail.com>
2011-04-09 15:24:27 +0800

    init checkin util/outputc_print.m

---
+++ util/outputc_print.m
@@ -0,0 +1,14 @@
+function outputc_print( outputc )
+%print info in output cell
+%   Detailed explanation goes here
+
+N=length(outputc);
+for i=1:N
+    output=outputc{i};
+    iter=output.iterations;
+    fc=output.funcCount;
+    step=output.stepsize;
+    grad=output.firstorderopt;
+    fprintf('%d\t%d\t%g\t%g\n',iter,fc,step,grad);
+end
+
