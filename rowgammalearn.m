a5ccdb64b47fde5c5da4279d75a1a8157fa4b353
Luyi Chen <lychen1109@gmail.com>
2011-04-10 17:16:01 +0800

    add historys as output to rowgammalearn

--- rowgammalearn.m
+++ rowgammalearn.m
@@ -1,4 +1,4 @@
-function [thetaa,fvala,exitflaga,outputc]=rowgammalearn(label,data,cvpa,thetaa_init)
+function [thetaa,fvala,exitflaga,outputc,historys]=rowgammalearn(label,data,cvpa,thetaa_init)
 %learn row rbf params based on single gamma result
 
 N=length(cvpa);
@@ -7,9 +7,12 @@ thetaa=zeros(N,n_theta);
 fvala=zeros(N,1);
 exitflaga=zeros(N,1);
 outputc=cell(N,1);
+historys=cell(N,1);
 
 for i=1:N
     fprintf('learning %dth split\n',i);
     cvp=cvpa(i);
-    [thetaa(i,:),fvala(i),exitflaga(i),outputc{i}]=paramlearnotb2(label(cvp.training),data(cvp.training,:),thetaa_init(i,:),@rowsvmfun,@rowparamgrad,@logistreg,@svmllhood,@svmoutputgrad);    
+    [thetaa(i,:),fvala(i),exitflaga(i),outputc{i},historys{i}]=paramlearnotb2(label(cvp.training),data(cvp.training,:),...
+                                                    label(cvp.test),data(cvp.test,:),thetaa_init(i,:),@rowsvmfun,@rowparamgrad,...
+                                                    @logistreg,@svmllhood,@svmoutputgrad);    
 end
