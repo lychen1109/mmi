46d56ce02f10ad0caf1cdb2e7a4c4c8487a80674
Luyi Chen <lychen1109@gmail.com>
2011-04-13 19:13:30 +0800

    rewrite crossdist to use new index

--- crossdist.m
+++ crossdist.m
@@ -3,40 +3,48 @@ function [ distmat ] = crossdist( A,B,gt )
 %   when A==B, the function use symetric of matrix to save half of the
 %   computing time
 
-NA=size(A,1);
-NB=size(B,1);
-distmat=zeros(NA*NB,1);
-idx=zeros(NA*NB,size(A,2),2);
-for i=1:NA
-    idx((i-1)*NB+(1:NB),:,1)=repmat(A(i,:),NB,1);
-    idx((i-1)*NB+(1:NB),:,2)=B;
-end
-
-if ~ isequal(A,B)
-    parfor i=1:NA*NB
-        distmat(i)=sampledist(idx(i,:,:),gt);
-    end
-    distmat=reshape(distmat,NB,NA)';
-else
-    lidx=tril(true(NA),-1);
-    subidx=idx(lidx,:,:);
-    n_subidx=size(subidx,1);
-    subdistmat=zeros(n_subidx,1);
-    parfor i=1:n_subidx
-        subdistmat(i)=sampledist(subidx(i,:,:),gt);
-    end
-    distmat(lidx)=subdistmat;
-    distmat=reshape(distmat,NB,NA)';
-    distmat=distmat+distmat';
-end
-
-function dist=sampledist(samples,gt)
-%calculate distance between two sample pairs
+%make sure gt is row vector
 if size(gt,2)==1
     gt=gt';
 end
-s1=samples(1,:,1);
-s2=samples(1,:,2);
-dist=norm(s1.*gt-s2.*gt)^2;
+
+NA=size(A,1);
+NB=size(B,1);
+distmat=zeros(NA,NB);
+
+%rescale A and B according to gt
+A=repmat(gt,NA,1).*A;
+B=repmat(gt,NB,1).*B;
+
+%prepare sample index
+idxA=repmat(1:NA,NB,1);
+idxB=repmat((1:NB)',1,NA);
+
+if ~ isequal(A,B)
+    parfor i=1:NA*NB
+        distmat(i)=sampledist(idxA(i),idxB(i),A,B);
+    end    
+else
+    lidx=tril(true(NA),-1);
+    lidxA=idxA(lidx);
+    lidxB=idxB(lidx);    
+    n_subidx=NA^2-NA/2;
+    ldistmat=distmat(lidx);
+    parfor i=1:n_subidx
+        ldistmat(i)=sampledist(lidxA(i),lidxB(i,A,B));
+    end
+    distmat(lidx)=ldistmat;    
+    distmat=distmat+distmat';
+end
+end
+
+    
+function dist=sampledist(iA,iB,A,B)
+%calculate distance between two sample pairs
+
+s1=A(iA,:);
+s2=B(iB,:);
+dist=norm(s1-s2)^2;
+end
 
 
