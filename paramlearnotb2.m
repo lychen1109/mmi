dd66152365092b1244884cf6230dde9217bdbb87
Luyi Chen <lychen1109@gmail.com>
2011-04-14 17:05:33 +0800

    add validation accuracy to output fun in paramlearnotb2

--- paramlearnotb2.m
+++ paramlearnotb2.m
@@ -9,6 +9,7 @@ cvp=cvpartition(labeltrain,'Kfold',K);
 history.thetas=[];
 history.fvals=[];
 history.accutests=[];
+history.accuvalis=[];
 
 opt=optimset('GradObj','on','LargeScale','off','display','iter-detailed','DerivativeCheck','off','diffmin',1e-2,...
             'Tolfun',1e-3,'tolx',1e-3,'outputfcn',@outfun);
@@ -21,7 +22,8 @@ opt=optimset('GradObj','on','LargeScale','off','display','iter-detailed','Deriva
         dvalues=zeros(n_data,1);
         ac=zeros(K,1);
         for i=1:K
-            [modelstructs(i),accu,dvalues(cvp.test(i))]=mysvmfun(labeltrain(cvp.training(i)),datatrain(cvp.training(i),:),labeltrain(cvp.test(i)),datatrain(cvp.test(i),:),theta);
+            [modelstructs(i),accu,dvalues(cvp.test(i))]=mysvmfun(labeltrain(cvp.training(i)),datatrain(cvp.training(i),:),...
+                                            labeltrain(cvp.test(i)),datatrain(cvp.test(i),:),theta);
             ac(i)=accu(1);
         end
         fprintf('average validation accuracy is %g (%g)\n',mean(ac),std(ac));
@@ -46,6 +48,14 @@ opt=optimset('GradObj','on','LargeScale','off','display','iter-detailed','Deriva
             history.fvals=[history.fvals;optimValues.fval];
             [~,accu,~]=mysvmfun(labeltrain,datatrain,labeltest,datatest,theta);
             history.accutests=[history.accutests;accu(1)];
+            
+            ac=zeros(K,1);
+            for i=1:K
+                [~,accu,~]=mysvmfun(labeltrain(cvp.training(i)),datatrain(cvp.training(i),:),labeltrain(cvp.test(i)),...
+                                datatrain(cvp.test(i),:),theta);
+                ac(i)=accu(1);
+            end
+            history.accuvalis=[history.accuvalis;mean(ac)];
         end
     end
 end
