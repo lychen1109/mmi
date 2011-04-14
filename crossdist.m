2a910ea4cac81528a7a3db7b3a9d70ff14790f7b
Luyi Chen <lychen1109@gmail.com>
2011-04-14 16:37:50 +0800

    update crossdist to support narg==2

--- crossdist.m
+++ crossdist.m
@@ -2,18 +2,20 @@ function [ distmat ] = crossdist( A,B,gt )
 %crossdist calculate cross distance between samples in A and B
 %   Detailed explanation goes here
 
-%make sure gt is row vector
-if size(gt,2)==1
-    gt=gt';
-end
-
 NA=size(A,1);
 NB=size(B,1);
 distmat=zeros(NA,NB);
 
-%rescale A and B according to gt
-A=repmat(gt,NA,1).*A;
-B=repmat(gt,NB,1).*B;
+if nargin==3
+    %make sure gt is row vector
+    if size(gt,2)==1
+        gt=gt';
+    end
+    
+    %rescale A and B according to gt
+    A=repmat(gt,NA,1).*A;
+    B=repmat(gt,NB,1).*B;
+end
 
 if ~ isequal(A,B)
     for i=1:NA
