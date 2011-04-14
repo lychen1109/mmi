c75773489715e61a6352a1ca8c7572d88593b8c9
Luyi Chen <lychen1109@gmail.com>
2011-04-14 12:37:02 +0800

    init checkin crossdist2

---
+++ crossdist2.m
@@ -0,0 +1,35 @@
+function [ distmat ] = crossdist2( A,B,gt )
+%crossdist2 use two loop to fill distmat
+%   Detailed explanation goes here
+
+%make sure gt is row vector
+if size(gt,2)==1
+    gt=gt';
+end
+
+NA=size(A,1);
+NB=size(B,1);
+distmat=zeros(NA,NB);
+
+%rescale A and B according to gt
+A=repmat(gt,NA,1).*A;
+B=repmat(gt,NB,1).*B;
+
+if ~ isequal(A,B)
+    for i=1:NA
+        for j=1:NB
+            distmat(i,j)=norm(A(i,:)-B(j,:))^2;
+        end
+    end
+else
+    for i=1:NA-1
+        for j=i+1:NA
+            distmat(i,j)=norm(A(i,:)-B(j,:))^2;
+        end
+    end
+    distmat=distmat+distmat';
+end
+
+
+end
+
