03b94d59a506c5dda1a25f911110bc7e26d3e570
Luyi Chen <lychen1109@gmail.com>
2011-04-15 16:59:41 +0800

    rewrite svmllhood to use t

--- svmllhood.m
+++ svmllhood.m
@@ -1,8 +1,22 @@
 function L=svmllhood(labelv,outputv,A,B)
 %likelihood of svm output
 
-outputvp=outputv(labelv==1);
-outputvn=outputv(labelv==0);
-Like1=log(1./(1+exp(A*outputvp+B)));
-Like2=log(1-1./(1+exp(A*outputvn+B)));
-L=-sum(Like1)-sum(Like2);
+%%%%%%%%%%%%%%%%%%%%%%
+% When t is not used
+%%%%%%%%%%%%%%%%%%%%%%%
+% outputvp=outputv(labelv==1);
+% outputvn=outputv(labelv==0);
+% Like1=log(1./(1+exp(A*outputvp+B)));
+% Like2=log(1-1./(1+exp(A*outputvn+B)));
+% L=-sum(Like1)-sum(Like2);
+
+t=zeros(size(labelv));
+s=svmlogist(labelv,outputv,A,B);
+
+labelv=2*labelv-1;
+np=sum(labelv==1);
+nn=sum(labelv==-1);
+t(labelv==1)=(np+1)/(np+2);
+t(labelv==-1)=(nn+1)/(nn+2);
+L=t.*log(s)+(1-t).*log(1-s);
+L=-sum(L);
