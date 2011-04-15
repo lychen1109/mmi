c82b4b24fb358fe4b0201ce950d05cfb19c9f987
Luyi Chen <lychen1109@gmail.com>
2011-04-15 17:20:45 +0800

    update svmllhood to use new t output

--- svmllhood.m
+++ svmllhood.m
@@ -10,13 +10,6 @@ function L=svmllhood(labelv,outputv,A,B)
 % Like2=log(1-1./(1+exp(A*outputvn+B)));
 % L=-sum(Like1)-sum(Like2);
 
-t=zeros(size(labelv));
-s=svmlogist(labelv,outputv,A,B);
-
-labelv=2*labelv-1;
-np=sum(labelv==1);
-nn=sum(labelv==-1);
-t(labelv==1)=(np+1)/(np+2);
-t(labelv==-1)=(nn+1)/(nn+2);
+[s,t]=svmlogist(labelv,outputv,A,B);
 L=t.*log(s)+(1-t).*log(1-s);
 L=-sum(L);
