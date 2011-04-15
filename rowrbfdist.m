f87b95bf98a46a2febac2c4fb0e6248b4fe7f549
Luyi Chen <lychen1109@gmail.com>
2011-04-15 10:59:56 +0800

    del rowrbfdist

--- rowrbfdist.m
+++
@@ -1,9 +0,0 @@
-function d=rowrbfdist(x,y,theta)
-%row rbf kernel distance weighted by kernel params
-
-d=0;
-for i=1:9
-    t=false(9,9);
-    t(i,:)=true(1,9);
-    d=d+2^theta(i+1)*norm(x(t)-y(t))^2;
-end
