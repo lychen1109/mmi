6b87564bd8736ad647cfaf1d08d2768ac9bdc276
Luyi Chen <lychen1109@gmail.com>
2011-04-15 10:19:03 +0800

    calculate full kernel matrix in groupsvmfun

--- groupsvmfun.m
+++ groupsvmfun.m
@@ -11,11 +11,23 @@ gt=sqrt(2.^gt);
 %rescale samples
 n_train=length(labeltrain);
 n_test=length(labeltest);
-datatrain=repmat(gt,n_train,1).*datatrain;
-datatest=repmat(gt,n_test,1).*datatest;
+%datatrain=repmat(gt,n_train,1).*datatrain;
+%datatest=repmat(gt,n_test,1).*datatest;
+
+%calculate training matrix
+ktrain=crossdist(datatrain,datatrain,gt);
+ktrain=exp(-ktrain);
 
 C=2^theta(1);
-cmd=['-c ' num2str(C) '-g 1'];
-model=svmtrain(labeltrain,datatrain,cmd);
-[~,accu,dvalues]=svmpredict(labeltest,datatest,model);
-modelstruct=modelparse(model,C);
+cmd=['-c ' num2str(C) ' -t 4'];
+model=svmtrain(labeltrain,[(1:n_train)' ktrain],cmd);
+modelstruct=rowmodelparse(model,datatrain,C);
+
+svidx=full(model.SVs);
+datasv=datatrain(svidx,:);
+ktest2=crossdist(datatest,datasv,gt);
+ktest=zeros(n_test,n_train);
+ktest(:,svidx)=ktest2;
+ktest=exp(-ktest);
+
+[~,accu,dvalues]=svmpredict(labeltest,[(1:n_test)' ktest],model);
