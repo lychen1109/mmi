aae9ed8cd5e1ff7ddde18925265a820303800d22
Luyi Chen <lychen1109@gmail.com>
2011-04-15 12:55:49 +0800

    update input/output

--- runparamlearn.m
+++ runparamlearn.m
@@ -1,13 +1,16 @@
-function [thetaarray,fvala,exitflaga]=singlegammalearn(label,data,cvpa,theta)
+function [thetas,fvals,exitflags,outputs,histories]=runparamlearn(label,data,cvpa,theta)
 %learn params for a set of spliters
 
 N=length(cvpa);
-thetaarray=zeros(N,length(theta));%every row is learned theta for one split
-fvala=zeros(N,1);
-exitflaga=zeros(N,1);
+thetas=zeros(N,length(theta));%every row is learned theta for one split
+fvals=zeros(N,1);
+exitflags=zeros(N,1);
+outputs=cell(N,1);
+histories=cell(N,1);
 
 for i=1:N
     fprintf('learning %dth split\n',i);
     cvp=cvpa(i);    
-    [thetaarray(i,:),fvala(i),exitflaga(i)]=paramlearnotb2(label(cvp.training),data(cvp.training,:),theta,@mysvmfun,@paramgrad,@logistreg,@svmllhood,@svmoutputgrad);    
+    [thetas(i,:),fvals(i),exitflags(i),outputs{i},histories{i}]=paramlearn(label(cvp.training),data(cvp.training,:),...
+                                                                label(cvp.test),data(cvp.test,:),theta);    
 end
