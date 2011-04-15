2aa18545440ab07c41df515d7d05e6cbe51f9f08
Luyi Chen <lychen1109@gmail.com>
2011-04-15 13:04:17 +0800

    update input output

--- runparamlearngroup.m
+++ runparamlearngroup.m
@@ -1,18 +1,17 @@
-function [thetaa,fvala,exitflaga,outputc,historys]=rowgammalearn(label,data,cvpa,thetaa_init)
-%learn row rbf params based on single gamma result
+function [thetas,fvals,exitflags,outputs,histories]=runparamlearngroup(label,data,cvpa,thetaa_init,group)
+%batch learn of group params
 
 N=length(cvpa);
 n_theta=size(thetaa_init,2);
-thetaa=zeros(N,n_theta);
-fvala=zeros(N,1);
-exitflaga=zeros(N,1);
-outputc=cell(N,1);
-historys=cell(N,1);
+thetas=zeros(N,n_theta);
+fvals=zeros(N,1);
+exitflags=zeros(N,1);
+outputs=cell(N,1);
+histories=cell(N,1);
 
 for i=1:N
     fprintf('learning %dth split\n',i);
     cvp=cvpa(i);
-    [thetaa(i,:),fvala(i),exitflaga(i),outputc{i},historys{i}]=paramlearnotb2(label(cvp.training),data(cvp.training,:),...
-                                                    label(cvp.test),data(cvp.test,:),thetaa_init(i,:),@rowsvmfun,@rowparamgrad,...
-                                                    @logistreg,@svmllhood,@svmoutputgrad);    
+    [thetas(i,:),fvals(i),exitflags(i),outputs{i},histories{i}]=paramlearngroup(label(cvp.training),data(cvp.training,:),...
+                                                    label(cvp.test),data(cvp.test,:),thetaa_init(i,:),group);    
 end
