cab32bc4b12440be6c2d26b5f0aad95d02c94714
Luyi Chen <lychen1109@gmail.com>
2011-04-15 17:19:07 +0800

    output s and t

--- svmlogist.m
+++ svmlogist.m
@@ -1,5 +1,11 @@
-function p=svmlogist(labelv,dvalues,A,B)
+function [s,t]=svmlogist(labelv,dvalues,A,B)
 %svm output regression
 
 labelv=2*labelv-1;
-p=1./(1+exp(labelv.*(A*dvalues+B)));
+s=1./(1+exp(labelv.*(A*dvalues+B)));
+
+t=zeros(size(labelv));
+np=sum(labelv==1);
+nn=sum(labelv==-1);
+t(labelv==1)=(np+1)/(np+2);
+t(labelv==-1)=(nn+1)/(nn+2);
