5fc84d5199d155745b183523a4ed8c2d60c86ab7
Luyi Chen <lychen1109@gmail.com>
2011-04-16 19:26:05 +0800

    minor fix

--- runsvmgrid.m
+++ runsvmgrid.m
@@ -1,12 +1,12 @@
-function thetaa=svmgridsets(label,feat,cvpa)
+function thetas=runsvmgrid(label,feat,cvpa)
 %a wrapper of grid search for a set of splits
 
 N=length(cvpa);
-thetaa=zeros(N,2);
-rangec=-2:8;
+thetas=zeros(N,2);
+rangec=8:-1:2;
 rangeg=-7:7;
 
 for i=1:N
     cvp=cvpa(i);
-    [thetaa(i,1),thetaa(i,2)]=svmgrid(label(cvp.training),feat(cvp.training,:),rangec,rangeg);
+    [thetas(i,1),thetas(i,2)]=svmgrid(label(cvp.training),feat(cvp.training,:),rangec,rangeg);
 end
