618bc57bd9e0a4fbcb37a45bbada5e4273836f2a
Luyi Chen <lychen1109@gmail.com>
2011-04-16 12:35:33 +0800

    output final accutest only

--- historyana.m
+++ historyana.m
@@ -1,12 +1,9 @@
-function [bestaccu finalaccu]=historyana(histories)
+function [finalaccu]=historyana(histories)
 %analysis history cells
 
 N=length(histories);
-bestaccu=zeros(N,1);
 finalaccu=zeros(N,1);
 for i=1:N
     history=histories{i};
-    finalaccu(i)=history.accutests(end);
-    [~,I]=max(history.accuvalis);
-    bestaccu(i)=history.accutests(I);
+    finalaccu(i)=history.accutests(end);    
 end
