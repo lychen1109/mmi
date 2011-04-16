e89a7ffe42eb4be8ade951617ee0967b5c457761
Luyi Chen <lychen1109@gmail.com>
2011-04-16 18:46:27 +0800

    rewrite svmgrid with index matrix

--- svmgrid.m
+++ svmgrid.m
@@ -3,39 +3,26 @@ function [bestc,bestg,bestcv,cv]=svmgrid(grpTrain,dataTrain,rangec,rangeg)
 
 nc=length(rangec);
 ng=length(rangeg);
-NP=nc*ng;
-fprintf('evaluating %d sets of params\n',NP);
-cv=zeros(NP,1);
-params=zeros(NP,2);
+cv=zeros(nc,ng);
+rangec=rangec(:);
+rangeg=rangeg(:);
+matc=repmat(rangec,1,ng);
+matg=repmat(rangeg',nc,1);
+matc=2.^matc;
+matg=2.^matg;
 
-for i=1:nc
-    wr=(i-1)*ng+(1:ng);%working range
-    params(wr,1)=rangec(i);
-    params(wr,2)=rangeg;
-end
-
-tic;
-parfor i=1:NP
-    [log2c,log2g]=paramsplit(params(i,:));    
-    cmd=['-v 5 -c ' num2str(2^log2c) ' -g ' num2str(2^log2g)];
+parfor i=1:length(cv(:))      
+    cmd=['-v 5 -c ' num2str(matc(i)) ' -g ' num2str(matg(i))];
     cv(i)=svmtrain(grpTrain,dataTrain,cmd);
 end
-t=toc;
-fprintf('param search finished in %g sec\n',t);
 
-[bestcv,I]=max(cv);
-[log2c,log2g]=paramsplit(params(I,:));
+[bestcv,I]=max(cv(:));
+bestc=matc(I);
+bestg=matg(I);
 
-bestc=2^log2c;
-bestg=2^log2g;
 
-cv=reshape(cv,ng,nc)';
 
-function [log2c,log2g]=paramsplit(prow)
-%split params
 
-log2c=prow(1);
-log2g=prow(2);
 
 
 
