941045458260babf843e23ab49aa6ac3a2956391
Luyi Chen <lychen1109@gmail.com>
2011-04-19 11:23:27 +0800

    init checkin groupsvmcv

---
+++ groupsvmcv.m
@@ -0,0 +1,20 @@
+function [mac,mnsv,mbsv]=groupsvmcv(label,feat,group,theta)
+%groupsvmcv returns cross validation accuracy, nsv, bsv
+
+cvp=cvpartition(label,'kfold',5);
+ac=zeros(5,1);
+nsv=zeros(5,1);
+bsv=zeros(5,1);
+for i=1:5
+    labeltrain=label(cvp.training(i));
+    datatrain=feat(cvp.training(i),:);
+    labeltest=label(cvp.test(i));
+    datatest=feat(cvp.test(i),:);
+    [modelstruct,accu]=groupsvmfun(labeltrain,datatrain,labeltest,datatest,group,theta);
+    ac(i)=accu(1);
+    nsv(i)=size(modelstruct.SVs,1);
+    bsv(i)=size(modelstruct.SVsc,1);
+end
+mac=mean(ac);
+mnsv=mean(nsv);
+mbsv=mean(bsv);
