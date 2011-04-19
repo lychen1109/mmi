04d4885ff0614d74b9b2144e50394ef41ec9e177
Luyi Chen <lychen1109@gmail.com>
2011-04-19 12:18:37 +0800

    update output interface of runsvmgrid3

--- runsvmgrid3.m
+++ runsvmgrid3.m
@@ -1,11 +1,15 @@
-function thetas=runsvmgrid3(label,feat,cvpa,group,rangec,rangeg1,rangeg2)
+function [thetas,bestcv,cvmats,nsvmats,bsvmats]=runsvmgrid3(label,feat,cvpa,group,rangec,rangeg1,rangeg2)
 %a wrapper of grid search for a set of splits
 
 N=length(cvpa);
-thetas=zeros(N,3);
+thetas=cell(N,1);
+bestcv=zeros(N,1);
+cvmats=cell(N,1);
+nsvmats=cell(N,1);
+bsvmats=cell(N,1);
 
 for i=1:N
     cvp=cvpa(i);
     fprintf('processing %dth split\n',i);
-    [thetas(i,1),thetas(i,2),thetas(i,3)]=svmgrid3(label(cvp.training),feat(cvp.training,:),group,rangec,rangeg1,rangeg2);
+    [thetas{i},bestcv(i),cvmats{1},nsvmats{1},bsvmats{1}]=svmgrid3(label(cvp.training),feat(cvp.training,:),group,rangec,rangeg1,rangeg2);
 end
