8f8cca1b17902361372c8c5c1b7850c298d1ab97
Luyi Chen <lychen1109@gmail.com>
2011-04-19 13:35:46 +0800

    update svmgfid to support nsv bsv output

--- runsvmgrid.m
+++ runsvmgrid.m
@@ -1,11 +1,15 @@
-function thetas=runsvmgrid(label,feat,cvpa,rangec,rangeg)
+function [thetas,bestcv,cvmat,nsvmat,bsvmat]=runsvmgrid(label,feat,cvpa,rangec,rangeg)
 %a wrapper of grid search for a set of splits
 
 N=length(cvpa);
-thetas=zeros(N,2);
+thetas=cell(N,1);
+bestcv=zeros(N,1);
+cvmat=cell(N,1);
+nsvmat=cell(N,1);
+bsvmat=cell(N,1);
 
 for i=1:N
     cvp=cvpa(i);
     fprintf('processing %dth split\n',i);
-    [thetas(i,1),thetas(i,2)]=svmgrid(label(cvp.training),feat(cvp.training,:),rangec,rangeg);
+    [thetas{1},bestcv(i),cvmat{i},nsvmat{i},bsvmat{i}]=svmgrid(label(cvp.training),feat(cvp.training,:),rangec,rangeg);
 end
--- svmgrid.m
+++ svmgrid.m
@@ -1,24 +1,23 @@
-function [bestc,bestg,bestcv,cv]=svmgrid(grpTrain,dataTrain,rangec,rangeg)
+function [theta,bestcv,cvmat,nsvmat,bsvmat]=svmgrid(grpTrain,dataTrain,rangec,rangeg)
 %parameter selection
 
 nc=length(rangec);
 ng=length(rangeg);
-cv=zeros(nc,ng);
+cvmat=zeros(nc,ng);
+nsvmat=zeros(nc,ng);
+bsvmat=zeros(nc,ng);
 rangec=rangec(:);
 rangeg=rangeg(:);
-matlog2c=repmat(rangec,1,ng);
-matlog2g=repmat(rangeg',nc,1);
-matc=2.^matlog2c;
-matg=2.^matlog2g;
+log2c=repmat(rangec,1,ng);
+log2g=repmat(rangeg',nc,1);
+theta=[log2c(:) log2g(:)];
 
-parfor i=1:nc*ng      
-    cmd=['-v 5 -c ' num2str(matc(i)) ' -g ' num2str(matg(i))];
-    cv(i)=svmtrain(grpTrain,dataTrain,cmd);
+parfor i=1:nc*ng    
+    [cvmat(i) nsvmat(i) bsvmat(i)]=mysvmcv(grpTrain,dataTrain,theta);
 end
 
-[bestcv,I]=max(cv(:));
-bestc=matlog2c(I);
-bestg=matlog2g(I);
+bestcv=max(cvmat(:));
+theta=theta(cvmat==bestcv,:);
 
 
 
