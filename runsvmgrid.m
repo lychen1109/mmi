89782d8f9833c0a4bc6af876df3b06e85bfbc4af
Luyi Chen <lychen1109@gmail.com>
2011-05-07 11:51:12 +0800

    simplfies output

--- runsvmgrid.m
+++ runsvmgrid.m
@@ -1,15 +1,13 @@
-function [thetas,bestcv,cvmat,nsvmat,bsvmat]=runsvmgrid(label,feat,cvpa,rangec,rangeg)
+function [thetas,bestcv]=runsvmgrid(label,feat,cvpa,rangec,rangeg)
 %a wrapper of grid search for a set of splits
 
 N=length(cvpa);
-thetas=cell(N,1);
+thetas=zeros(N,2);
 bestcv=zeros(N,1);
-cvmat=cell(N,1);
-nsvmat=cell(N,1);
-bsvmat=cell(N,1);
 
 for i=1:N
     cvp=cvpa(i);
     fprintf('processing %dth split\n',i);
-    [thetas{i},bestcv(i),cvmat{i},nsvmat{i},bsvmat{i}]=svmgrid(label(cvp.training),feat(cvp.training,:),rangec,rangeg);
+    [thetas(i,:),bestcv(i)]=svmgrid(label(cvp.training),feat(cvp.training,:),rangec,rangeg);
+    fprintf('bestcv is %g\n',bestcv(i));
 end
