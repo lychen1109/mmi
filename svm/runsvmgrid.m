9e13560270330d8477e8f4f67efc6827c7fa0b4d
Luyi Chen <lychen1109@gmail.com>
2011-05-08 10:28:00 +0800

    change runsvmgrid to run multiple times

--- svm/runsvmgrid.m
+++ svm/runsvmgrid.m
@@ -1,13 +1,17 @@
-function [thetas,bestcv]=runsvmgrid(label,feat,cvpa,rangec,rangeg)
+function [thetas]=runsvmgrid(label,feat,cvpa,rangec,rangeg)
 %a wrapper of grid search for a set of splits
 
 N=length(cvpa);
+K=5; %number of cv runed per split
 thetas=zeros(N,2);
-bestcv=zeros(N,1);
 
 for i=1:N
     cvp=cvpa(i);
     fprintf('processing %dth split\n',i);
-    [thetas(i,:),bestcv(i)]=svmgrid(label(cvp.training),feat(cvp.training,:),rangec,rangeg);
-    fprintf('bestcv is %g\n',bestcv(i));
+    thetatmp=zeros(K,2);    
+    for k=1:K
+        [thetatmp(k,:)]=svmgrid(label(cvp.training),feat(cvp.training,:),rangec,rangeg);        
+    end
+    thetas(i,:)=median(thetatmp);    
+    fprintf('best params chose for the split is (%d,%d)\n\n',thetas(i,1),thetas(i,2));
 end
