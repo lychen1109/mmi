be065d590fad11b83fa05d662eb4a1cbe7b3fea3
Luyi Chen <lychen1109@gmail.com>
2011-05-16 11:20:05 +0800

    change dh2

--- feature_ext/dh2.m
+++ feature_ext/dh2.m
@@ -2,9 +2,9 @@ function dhist=dh2(img,T)
 %difference histogram of jpg_imgs
 
 [N,M]=size(img);
-diffimg=img(:,1:M-1)-img(:,2:M);
+diffimg=img(:,1:M-2)-img(:,3:M);
 diffimg(diffimg>T)=T;
 diffimg(diffimg<-T)=-T;
 X=-T:T;
 dhist=hist(diffimg(:),X);
-dhist=dhist/(N*(M-1));
+dhist=dhist/(N*(M-2));
