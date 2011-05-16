f24a3ddad58c4f34c5aa3379a1248514d38723c9
Luyi Chen <lychen1109@gmail.com>
2011-05-16 10:38:43 +0800

    change dhgen to use dh2

--- feature_ext/dhgen.m
+++ feature_ext/dhgen.m
@@ -2,11 +2,11 @@ function feat=dhgen(imgs,T)
 %difference histogram feature generation
 
 N=size(imgs,1);
-feat=zeros(N,4*T+1);
+feat=zeros(N,2*T+1);
 
 for i=1:N
     img=imgs(i,:);
     img=reshape(img,128,128);
-    dhist=dh(img,T);
+    dhist=dh2(img,T);
     feat(i,:)=dhist(:)';
 end
