ee25e375b3e998b0a14d4d3ba8704f1b5fcbe1d5
Luyi Chen <lychen1109@gmail.com>
2011-05-23 11:39:43 +0800

    change dhgen to use tpm3d

--- feature_ext/dhgen.m
+++ feature_ext/dhgen.m
@@ -2,11 +2,11 @@ function feat=dhgen(imgs,T)
 %difference histogram feature generation
 
 N=size(imgs,1);
-feat=zeros(N,4*T+1);
+feat=zeros(N,(2*T+1)^3);
 
 for i=1:N
     img=imgs(i,:);
     img=reshape(img,128,128);
-    dhist=markov3(img,T);
+    dhist=tpm3d(img,T);
     feat(i,:)=dhist(:)';
 end
