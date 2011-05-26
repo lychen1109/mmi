86c85260943d6c2c49d94d92ac650c8ec9089801
Luyi Chen <lychen1109@gmail.com>
2011-05-26 14:40:33 +0800

    change pca2 to without threshold

--- feature_ext/featpca2.m
+++ feature_ext/featpca2.m
@@ -2,10 +2,10 @@ function h=featpca2(img,T)
 %2nd pca filter for 2nd degree markov transition
 
 y=img(:,1:end-1)-img(:,2:end);
-y(y>T)=T;
-y(y<-T)=-T;
+% y(y>T)=T;
+% y(y<-T)=-T;
 s=2^-0.5;
 y=s*(y(:,1:end-2)-y(:,3:end));
-B=floor(s*2*T)-1;
+B=T;
 h=hist(y(:),-B:B);
 h=h/(128*126);
