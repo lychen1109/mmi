e5d8f42f7b1c438a78bd4bde6f9fcec808f79724
Luyi Chen <lychen1109@gmail.com>
2011-05-26 13:28:46 +0800

    add T input to featpca1b

--- feature_ext/featpca1b.m
+++ feature_ext/featpca1b.m
@@ -1,4 +1,4 @@
-function h=featpca1b(img)
+function h=featpca1b(img,T)
 %pca1 feature without threshold
 
 y=img(:,1:127)-img(:,2:end);
@@ -7,6 +7,6 @@ y2=y(:,2:end-1);
 y3=y(:,3:end);
 s=2^-0.5;
 z=-0.5*y1+s*y2-0.5*y3;
-B=8;
+B=T;
 h=hist(z(:),-B:B);
 h=h/length(z(:));
