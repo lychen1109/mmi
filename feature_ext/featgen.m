3c2d3a4a85d37678c1d80c744633d371a768bd5d
Luyi Chen <lychen1109@gmail.com>
2011-05-28 19:44:16 +0800

    rewrite featgen to use 2nd order markov feature

--- feature_ext/featgen.m
+++ feature_ext/featgen.m
@@ -2,12 +2,13 @@ function feat=featgen(imgs)
 %feature generation for all images
 
 N=size(imgs,1);
-feat=zeros(N,23+23);
+feat=zeros(N,77);
 
 for i=1:N
     img=imgs(i,:);
     img=reshape(img,128,128);
-    h1=dh(img,8);
-    h2=sumhist(img,8);
-    feat(i,:)=[h1(:)' h2(:)'];
+    h1=featpca1(img,8);
+    h2=featpca2(img,8);
+    h3=featpca3(img,8);
+    feat(i,:)=[h1(:)' h2(:)' h3(:)'];
 end
