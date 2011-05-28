29ae3ec5993b7ebea65fc055651e10a4a80d2785
Luyi Chen <lychen1109@gmail.com>
2011-05-28 17:54:39 +0800

    rewrite featgen to use dh and sumhist

--- feature_ext/featgen.m
+++ feature_ext/featgen.m
@@ -2,12 +2,12 @@ function feat=featgen(imgs)
 %feature generation for all images
 
 N=size(imgs,1);
-feat=zeros(N,33+33);
+feat=zeros(N,23+23);
 
 for i=1:N
     img=imgs(i,:);
     img=reshape(img,128,128);
-    h1=dh2(img,16);
-    h2=sumhist2(img,16);
+    h1=dh(img,8);
+    h2=sumhist(img,8);
     feat(i,:)=[h1(:)' h2(:)'];
 end
