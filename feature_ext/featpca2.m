61f2a1592dc2dd86578674661a901d048b89c674
Luyi Chen <lychen1109@gmail.com>
2011-05-28 19:39:54 +0800

    rewrite featpca2

--- feature_ext/featpca2.m
+++ feature_ext/featpca2.m
@@ -2,10 +2,9 @@ function h=featpca2(img,T)
 %2nd pca filter for 2nd degree markov transition
 
 y=img(:,1:end-1)-img(:,2:end);
-% y(y>T)=T;
-% y(y<-T)=-T;
-s=2^-0.5;
-y=s*(y(:,1:end-2)-y(:,3:end));
-B=T;
+y(y>T)=T;
+y(y<-T)=-T;
+y=0.71*(y(:,1:end-2)-y(:,3:end));
+B=floor(0.71*2*T);
 h=hist(y(:),-B:B);
-h=h/(128*126);
+h=h/(length(y(:)));
