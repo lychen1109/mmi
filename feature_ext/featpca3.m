55431351a55bed8b413c1f20a2b1327f84cd60ef
Luyi Chen <lychen1109@gmail.com>
2011-05-28 19:41:40 +0800

    rewrite featpca3

--- feature_ext/featpca3.m
+++ feature_ext/featpca3.m
@@ -2,9 +2,12 @@ function h=featpca3(img,T)
 %pca 3rd component of 2nd order markov feature
 
 y=img(:,1:end-1)-img(:,2:end);
+y(y>T)=T;
+y(y<-T)=-T;
 y1=y(:,1:end-2);
 y2=y(:,2:end-1);
 y3=y(:,3:end);
 z=0.5*y1+0.71*y2+0.5*y3;
-h=hist(z(:),-T:T);
+B=floor(1.71*T);
+h=hist(z(:),-B:B);
 h=h/length(z(:));
