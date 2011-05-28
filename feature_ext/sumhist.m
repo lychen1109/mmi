03ff75b2fe6dbf052d968539ff96a4841c0febfb
Luyi Chen <lychen1109@gmail.com>
2011-05-28 17:51:43 +0800

    normalize vector to 1

--- feature_ext/sumhist.m
+++ feature_ext/sumhist.m
@@ -5,8 +5,8 @@ function shist=sumhist(img,T)
 diffimg=img(:,1:M-1)-img(:,2:M);
 diffimg(diffimg>T)=T;
 diffimg(diffimg<-T)=-T;
-sumimg=diffimg(:,1:M-2)+diffimg(:,2:M-1);
+sumimg=0.71*(diffimg(:,1:M-2)+diffimg(:,2:M-1));
 clear diffimg;
-X=-2*T:2*T;
-shist=hist(sumimg(:),X);
+B=floor(0.71*2*T);
+shist=hist(sumimg(:),-B:B);
 shist=shist/(N*(M-2));
