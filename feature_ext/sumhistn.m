9763172109efc33972ecbbb9d5974c4bc65ff55b
Luyi Chen <lychen1109@gmail.com>
2011-05-30 11:26:30 +0800

    no dc no threshold

--- feature_ext/sumhistn.m
+++ feature_ext/sumhistn.m
@@ -4,8 +4,8 @@ function shist=sumhistn(img,T)
 img=dcnan(img);
 [~,M]=size(img);
 diffimg=img(:,1:M-1)-img(:,2:M);
-diffimg(diffimg>T)=T;
-diffimg(diffimg<-T)=-T;
+%diffimg(diffimg>T)=T;
+%diffimg(diffimg<-T)=-T;
 sumimg=0.71*(diffimg(:,1:M-2)+diffimg(:,2:M-1));
 clear diffimg;
 B=floor(0.71*2*T);
