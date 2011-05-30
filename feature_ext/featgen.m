af3698b93b372d7a62bd912952db825c98c7d768
Luyi Chen <lychen1109@gmail.com>
2011-05-30 10:32:51 +0800

    rewrite featgen

--- feature_ext/featgen.m
+++ feature_ext/featgen.m
@@ -2,13 +2,12 @@ function feat=featgen(imgs)
 %feature generation for all images
 
 N=size(imgs,1);
-feat=zeros(N,77);
+feat=zeros(N,46);
 
 for i=1:N
     img=imgs(i,:);
     img=reshape(img,128,128);
-    h1=featpca1(img,8);
-    h2=featpca2(img,8);
-    h3=featpca3(img,8);
-    feat(i,:)=[h1(:)' h2(:)' h3(:)'];
+    h1=dhn(img,8);
+    h2=sumhistn(img,8);    
+    feat(i,:)=[h1(:)' h2(:)'];
 end
