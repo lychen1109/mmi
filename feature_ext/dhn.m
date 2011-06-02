c286127244b95ddee971ec49c18c475e683e0145
Luyi Chen <lychen1109@gmail.com>
2011-06-02 14:48:27 +0800

    add threshold

--- feature_ext/dhn.m
+++ feature_ext/dhn.m
@@ -4,8 +4,8 @@ function dhist=dhn(img,T)
 img=dcnan(img);
 [~,M]=size(img);
 diffimg=img(:,1:M-1)-img(:,2:M);
-%diffimg(diffimg>T)=T;
-%diffimg(diffimg<-T)=-T;
+diffimg(diffimg>T)=T;
+diffimg(diffimg<-T)=-T;
 diffimg2=0.71*(diffimg(:,1:M-2)-diffimg(:,2:M-1));
 clear diffimg;
 B=floor(0.71*2*T);
