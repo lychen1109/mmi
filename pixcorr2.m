b52d1d1e038ea79da52c3aff4016339f8610b59d
Luyi Chen <lychen1109@gmail.com>
2011-06-12 11:28:35 +0800

    bug fix

--- pixcorr2.m
+++ pixcorr2.m
@@ -4,8 +4,8 @@ function r=pixcorr2(img,T)
 Y=img(:,1:end-1)-img(:,2:end);
 Y(Y>T)=T;
 Y(Y<-T)=-T;
-Z1=Y(:,1:end-2,:);
-Z2=Y(:,2:end-1,:);
+Z1=Y(:,1:end-1);
+Z2=Y(:,2:end);
 cm=corr([Z1(:) Z2(:)]);
 r=cm(1,2);
 
