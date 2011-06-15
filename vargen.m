6b15353d5d539bde3522720a02cb57b02ad7da73
Luyi Chen <lychen1109@gmail.com>
2011-06-15 17:10:02 +0800

    rewrite vargen

--- vargen.m
+++ vargen.m
@@ -1,11 +1,10 @@
-function r=pixcorr2(img,T)
-%pixel correlation of difference image
+function Z=vargen(img,T)
+%adjacent variable generation from bdctimg
 
 Y=img(:,1:end-1)-img(:,2:end);
 Y(Y>T)=T;
 Y(Y<-T)=-T;
-Z1=Y(:,1:end-2);
-Z2=Y(:,3:end);
-cm=corr([Z1(:) Z2(:)]);
-r=cm(1,2);
+Z1=Y(:,1:end-1);
+Z2=Y(:,2:end);
+Z=[Z1(:) Z2(:)];
 
