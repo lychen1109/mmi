9efa0135c1c062d67407cd84849807b5ed055ce4
Luyi Chen <lychen1109@gmail.com>
2011-06-17 14:44:22 +0800

    rewirte svmtest

--- svm/svmtest.m
+++ svm/svmtest.m
@@ -1,17 +1,34 @@
-function [ac,nSV]=svmtest(class,data,cvpa,thetaa)
+function [ac,nSV]=svmtest(class,data,cvpa)
 %test the svm accuracy with k split
 
 k=length(cvpa);
 ac=zeros(k,1);
 nSV=zeros(k,1);
+thetas=zeros(5,2);
 
-for i=1:k    
+for i=1:5    
     CVP=cvpa(i);
     dataTrain=data(CVP.training,:);    
     grpTrain=class(CVP.training);
     dataTest=data(CVP.test,:);
     grpTest=class(CVP.test);
-    [modelstruct,accu,~]=mysvmfun(grpTrain,dataTrain,grpTest,dataTest,thetaa(i,:));
+    thetas(i,:)=svmgrid(grpTrain,dataTrain,12:-1:0,-6:6);
+    [modelstruct,accu,~]=mysvmfun(grpTrain,dataTrain,grpTest,dataTest,thetas(i,:));
+    nSV(i)=size(modelstruct.SVs,1);    
+    Nc=length(modelstruct.Yc);    
+    ac(i)=accu(1);
+    fprintf('accuracy %g with nSV %d,bSV %d\n',ac(i),nSV(i),Nc);
+end
+
+theta=median(thetas);
+fprintf('thetas selected for the rests: C=%d, g=%d\n',theta(1),theta(2));
+for i=6:k
+    CVP=cvpa(i);
+    dataTrain=data(CVP.training,:);    
+    grpTrain=class(CVP.training);
+    dataTest=data(CVP.test,:);
+    grpTest=class(CVP.test);    
+    [modelstruct,accu,~]=mysvmfun(grpTrain,dataTrain,grpTest,dataTest,theta);
     nSV(i)=size(modelstruct.SVs,1);    
     Nc=length(modelstruct.Yc);    
     ac(i)=accu(1);
