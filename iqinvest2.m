84ab9a7f1c9d068ee8baa46b3e9db5c52868f0cd
Luyi Chen <lychen1109@gmail.com>
2011-07-12 16:05:59 +0800

    output mean of samples

--- iqinvest2.m
+++ iqinvest2.m
@@ -8,8 +8,12 @@ bdctimgs=reshape(bdctimgs',128,128,K);
 y=bdctimgs(:,1:end-1,:)-bdctimgs(:,2:end,:);
 y(y>T)=T;
 y(y<-T)=-T;
+
 quart=quantile(y(:),[0.25 0.75]);
 fprintf('1st and 3rd quantile of samples are: %g, %g\n',quart(1),quart(2));
+
+m=mean(y(:));
+fprintf('Mean of samples are %g\n',m);
 
 IQ=quart(2)-quart(1);
 fprintf('IQ=%g\n',IQ);
