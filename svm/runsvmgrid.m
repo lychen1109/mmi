35ca2c97f622dea2a3b047a8b349c960dcf67111
Luyi Chen <lychen1109@gmail.com>
2011-07-14 17:21:00 +0800

    rewrite runsvmgrid

--- svm/runsvmgrid.m
+++ svm/runsvmgrid.m
@@ -1,17 +1,16 @@
-function [thetas]=runsvmgrid(label,feat,cvpa,rangec,rangeg)
+function [thetas,bestcv]=runsvmgrid(label,feat,cvpa,rangec,rangeg)
 %a wrapper of grid search for a set of splits
 
-N=length(cvpa);
-K=5; %number of cv runed per split
-thetas=zeros(N,2);
+N=5; %only use the first five splits for grid search
+thetas=[];
+bestcv=[];
 
 for i=1:N
     cvp=cvpa(i);
-    fprintf('processing %dth split\n',i);
-    thetatmp=zeros(K,2);    
-    for k=1:K
-        [thetatmp(k,:)]=svmgrid(label(cvp.training),feat(cvp.training,:),rangec,rangeg);        
-    end
-    thetas(i,:)=median(thetatmp);    
-    fprintf('best params chose for the split is (%d,%d)\n\n',thetas(i,1),thetas(i,2));
+    fprintf('processing %dth split\n',i);    
+    [thetatmp,bestcvtmp]=svmgrid2(label(cvp.training),feat(cvp.training,:),rangec,rangeg);
+    L=size(thetatmp,1);
+    bestcvtmp=ones(L,1)*bestcvtmp;
+    thetas=[thetas; thetatmp];
+    bestcv=[bestcv;bestcvtmp];
 end
