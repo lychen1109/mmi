47758d2f0eda99af2b11815c2b60e0d608641ef4
Luyi Chen <lychen1109@gmail.com>
2011-07-14 11:06:55 +0800

    update featgen to use cooccount

--- feature_ext/featgen.m
+++ feature_ext/featgen.m
@@ -1,14 +1,12 @@
-function feat=featgen(imgs)
+function feat=featgen(bdctimgs,edges)
 %feature generation for all images
 
-N=size(imgs,1);
-feat=zeros(N,77);
+N=size(bdctimgs,1);
+feat=zeros(N,11^2);
 
 for i=1:N
-    img=imgs(i,:);
-    img=reshape(img,128,128);
-    h1=featpca1(img,8);
-    h2=featpca2(img,8);
-    h3=featpca3(img,8);
-    feat(i,:)=[h1(:)' h2(:)' h3(:)'];
+    bdctimg=bdctimgs(i,:);
+    bdctimg=reshape(bdctimg,128,128);
+    cmat=cooccount(bdctimg,edges);
+    feat(i,:)=cmat(:)';
 end
