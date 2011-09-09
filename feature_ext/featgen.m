3bb13536798663ae36937fb09df937ac09ec9a63
Luyi Chen <lychen1109@gmail.com>
2011-09-09 14:37:00 +0800

    change loop function

--- feature_ext/featgen.m
+++ feature_ext/featgen.m
@@ -2,11 +2,11 @@ function feat=featgen(bdctimgs)
 %feature generation for all images
 
 N=size(bdctimgs,1);
-feat=zeros(N,91);
+feat=zeros(N,168);
 
 for i=1:N
     bdctimg=bdctimgs(i,:);
     bdctimg=reshape(bdctimg,128,128);
-    h=hffeat(bdctimg);
+    h=featmoment(bdctimg);
     feat(i,:)=h;
 end
