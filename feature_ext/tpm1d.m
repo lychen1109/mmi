28831d1a7edd8d10064c570fdb11b81a964769e5
Luyi Chen <lychen1109@gmail.com>
2011-09-10 11:20:56 +0800

    add new param to tpm1d: normalize

--- feature_ext/tpm1d.m
+++ feature_ext/tpm1d.m
@@ -1,10 +1,15 @@
-function D=tpm1d(img,T)
+function D=tpm1d(img,T,trans,normalize)
 %tmp horizontal with threshold
 %start from pixel value
+%if trans==1, transpose the matrix
 
-[N,M]=size(img);
 img=blkproc(img,[8 8],@dct2);
 img=round(abs(img));
+if trans==1
+    img=img';
+end
+
+[N,M]=size(img);
 
 diffimg=img(:,1:M-1)-img(:,2:M);
 
@@ -20,14 +25,16 @@ for i=1:N
     end
 end
 
-% for i=1:msize
-%     s=sum(D(i,:));
-%     if s>0
-%         D(i,:)=D(i,:)/s;
-%     end
-% end
-
-D=D/N/(M-2);
+if normalize==1
+    for i=1:msize
+        s=sum(D(i,:));
+        if s>0
+            D(i,:)=D(i,:)/s;
+        end
+    end
+else
+    D=D/N/(M-2);
+end
 
 
 
