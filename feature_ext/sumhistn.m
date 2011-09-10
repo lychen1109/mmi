8c6ae18d2882a3d5441d3912aafc276dbb645a68
Luyi Chen <lychen1109@gmail.com>
2011-09-10 14:27:54 +0800

    modify sumhistn

--- feature_ext/sumhistn.m
+++ feature_ext/sumhistn.m
@@ -1,14 +1,17 @@
 function shist=sumhistn(img,T)
 %sum histogram feature extraction
 
+img=img-mean(img(:));
+img=blkproc(img,[8 8],@dct2);
+img=abs(img);
 img=dcnan(img);
+
 [~,M]=size(img);
 diffimg=img(:,1:M-1)-img(:,2:M);
 diffimg(diffimg>T)=T;
 diffimg(diffimg<-T)=-T;
-sumimg=0.71*(diffimg(:,1:M-2)+diffimg(:,2:M-1));
+sumimg=diffimg(:,1:M-2)+diffimg(:,2:M-1);
 clear diffimg;
-B=floor(0.71*2*T);
-shist=hist(sumimg(:),-B:B);
+shist=hist(sumimg(:),-2*T:2*T);
 L=sum(~isnan(sumimg(:)));
 shist=shist/L;
