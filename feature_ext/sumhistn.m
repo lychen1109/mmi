3c87533280c15c8635c7fb2aef214937974f5bbf
Luyi Chen <lychen1109@gmail.com>
2011-09-10 14:42:32 +0800

    combine difference histogram into sumhistn

--- feature_ext/sumhistn.m
+++ feature_ext/sumhistn.m
@@ -1,4 +1,4 @@
-function shist=sumhistn(img,T)
+function shist=sumhistn(img,T,f)
 %sum histogram feature extraction
 
 img=img-mean(img(:));
@@ -10,7 +10,12 @@ img=dcnan(img);
 diffimg=img(:,1:M-1)-img(:,2:M);
 diffimg(diffimg>T)=T;
 diffimg(diffimg<-T)=-T;
-sumimg=diffimg(:,1:M-2)+diffimg(:,2:M-1);
+
+if f==0
+    sumimg=diffimg(:,1:M-2)+diffimg(:,2:M-1);
+else
+    sumimg=diffimg(:,1:M-2)-diffimg(:,2:M-1);
+end
 clear diffimg;
 shist=hist(sumimg(:),-2*T:2*T);
 L=sum(~isnan(sumimg(:)));
