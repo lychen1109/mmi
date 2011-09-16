176b6fa95e4f256c576ea54a37d94af58c3dd4b1
Luyi Chen <lychen1109@gmail.com>
2011-09-16 14:13:59 +0800

    init checkin of name2path

---
+++ name2path.m
@@ -0,0 +1,10 @@
+function p=name2path(filename)
+%filename to path transform
+
+root='C:\data\ImSpliceDataset\';
+lastunderscore=find(filename=='_',1,'last');
+prefix=filename(1:lastunderscore-1);
+prefix(prefix=='_')='-';
+prefix=lower(prefix);
+p=[root prefix filesep filename];
+        
