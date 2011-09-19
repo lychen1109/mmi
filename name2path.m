55d285fe517a9b91db2071c5c93c60339eb55f12
Luyi Chen <lychen1109@gmail.com>
2011-09-19 16:49:35 +0800

    fix for au-s-h

--- name2path.m
+++ name2path.m
@@ -6,5 +6,9 @@ lastunderscore=find(filename=='_',1,'last');
 prefix=filename(1:lastunderscore-1);
 prefix(prefix=='_')='-';
 prefix=lower(prefix);
+if isequal(prefix,'au-s-h')
+    prefix='au-ss-h';
+end
+
 p=[root prefix filesep filename];
         
