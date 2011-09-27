5feb93bf34f13357479e6dd98f7e68d33ad5bb56
Luyi Chen <lychen1109@gmail.com>
2011-09-27 09:54:04 +0800

    name2path support change root

--- name2path.m
+++ name2path.m
@@ -1,7 +1,16 @@
-function [p,prefix]=name2path(filename)
+function [p,prefix]=name2path(filename,varargin)
 %filename to path transform
 
 root='C:\data\ImSpliceDataset\';
+nvar=length(varargin);
+for i=1:nvar/2
+    pname=varargin{2*i-1};
+    switch pname
+        case 'root'
+            root=varargin{2*i};
+    end
+end
+
 lastunderscore=find(filename=='_',1,'last');
 prefix=filename(1:lastunderscore-1);
 prefix(prefix=='_')='-';
