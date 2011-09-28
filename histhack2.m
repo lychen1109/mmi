ceef1498a1b5f48658b40a1ee8e01e23a0d32a9f
Luyi Chen <lychen1109@gmail.com>
2011-09-28 15:28:26 +0800

    prevent calculating tpm all the time

--- histhack2.m
+++ histhack2.m
@@ -20,10 +20,11 @@ aimg=double(aimg);
 simg=double(simg);
 
 tm1=tpm1d(aimg,4,0,0);
-tm2=tpm1d(simg,4,0,0);
 bdctimg=blkproc(simg,[8 8],@dct2);
 bdctimg_ori=bdctimg;
 bdctimg=round(abs(bdctimg));
+tm2=tpm1(bdctimg,4);
+tm3=tm2;
 modified=false(size(bdctimg));%record if a coef has been modified
 
 NMOD=500; %maximum allowed number of modified coeff
@@ -34,8 +35,7 @@ if DEBUG
 end
 n_mod=0; %number of modified coef
 
-while n_mod<NMOD
-    tm3=tpm1(bdctimg,4);       
+while n_mod<NMOD        
     mahsdistpre=mahsdistcalc(tm1,tm3,sigma);
     if DEBUG
         recidx=recidx+1;
@@ -66,49 +66,49 @@ while n_mod<NMOD
         while loc_idx<=num
             loc=locations(randidx(loc_idx));
             [sj,sk]=ind2sub(size(bdctimg),loc);
-            bdctimgc=zeros(128,128,7);
+            %bdctimgc=zeros(128,128,7);
             mahsdist=10*ones(1,7);
             if bdctimg(sj,sk)>=2 && ~modified(sj,sk)
-                bdctimgc(:,:,1)=bdctmod(bdctimg,sj,sk,[1 0 0]);
-                tmc=tpm1(bdctimgc(:,:,1),4);
-                mahsdist(1)=mahsdistcalc(tm1,tmc,sigma);
+                %bdctimgc(:,:,1)=bdctmod(bdctimg,sj,sk,[1 0 0]);
+                tmc(:,:,1)=tmmod(bdctimg,tm3,sj,sk,[1 0 0]);
+                mahsdist(1)=mahsdistcalc(tm1,tmc(:,:,1),sigma);
             end
             
             if bdctimg(sj,sk+1)>=2 && ~modified(sj,sk+1)
-                bdctimgc(:,:,2)=bdctmod(bdctimg,sj,sk,[0 1 0]);
-                tmc=tpm1(bdctimgc(:,:,2),4);
-                mahsdist(2)=mahsdistcalc(tm1,tmc,sigma);
+                %bdctimgc(:,:,2)=bdctmod(bdctimg,sj,sk,[0 1 0]);
+                tmc(:,:,2)=tmmod(bdctimg,tm3,sj,sk,[0 1 0]);
+                mahsdist(2)=mahsdistcalc(tm1,tmc(:,:,2),sigma);
             end
             
             if bdctimg(sj,sk+2)>=2 && ~modified(sj,sk+2)
-                bdctimgc(:,:,3)=bdctmod(bdctimg,sj,sk,[0 0 1]);
-                tmc=tpm1(bdctimgc(:,:,3),4);
-                mahsdist(3)=mahsdistcalc(tm1,tmc,sigma);
+                %bdctimgc(:,:,3)=bdctmod(bdctimg,sj,sk,[0 0 1]);
+                tmc(:,:,3)=tmmod(bdctimg,tm3,sj,sk,[0 0 1]);
+                mahsdist(3)=mahsdistcalc(tm1,tmc(:,:,3),sigma);
             end
             
             if bdctimg(sj,sk)>=2 && bdctimg(sj,sk+1)>=2 && ~modified(sj,sk) && ~modified(sj,sk+1)
-                bdctimgc(:,:,4)=bdctmod(bdctimg,sj,sk,[1 1 0]);
-                tmc=tpm1(bdctimgc(:,:,4),4);
-                mahsdist(4)=mahsdistcalc(tm1,tmc,sigma);
+                %bdctimgc(:,:,4)=bdctmod(bdctimg,sj,sk,[1 1 0]);
+                tmc(:,:,4)=tmmod(bdctimg,tm3,sj,sk,[1 1 0]);
+                mahsdist(4)=mahsdistcalc(tm1,tmc(:,:,4),sigma);
             end
             
             if bdctimg(sj,sk+1)>=2 && bdctimg(sj,sk+2)>=2 && ~modified(sj,sk+1) && ~modified(sj,sk+2)
-                bdctimgc(:,:,5)=bdctmod(bdctimg,sj,sk,[0 1 1]);
-                tmc=tpm1(bdctimgc(:,:,5),4);
-                mahsdist(5)=mahsdistcalc(tm1,tmc,sigma);
+                %bdctimgc(:,:,5)=bdctmod(bdctimg,sj,sk,[0 1 1]);
+                tmc(:,:,5)=tmmod(bdctimg,tm3,sj,sk,[0 1 1]);
+                mahsdist(5)=mahsdistcalc(tm1,tmc(:,:,5),sigma);
             end
             
             if bdctimg(sj,sk)>=2 && bdctimg(sj,sk+2)>=2 && ~modified(sj,sk) && ~modified(sj,sk+2)
-                bdctimgc(:,:,6)=bdctmod(bdctimg,sj,sk,[1 0 1]);
-                tmc=tpm1(bdctimgc(:,:,6),4);
-                mahsdist(6)=mahsdistcalc(tm1,tmc,sigma);
+                %bdctimgc(:,:,6)=bdctmod(bdctimg,sj,sk,[1 0 1]);
+                tmc(:,:,6)=tmmod(bdctimg,tm3,sj,sk,[1 0 1]);
+                mahsdist(6)=mahsdistcalc(tm1,tmc(:,:,6),sigma);
             end
             
             if bdctimg(sj,sk)>=2 && bdctimg(sj,sk+1)>=2 && bdctimg(sj,sk+2)>=2 && ...
                     ~modified(sj,sk) && ~modified(sj,sk+1) && ~modified(sj,sk+2)
-                bdctimgc(:,:,7)=bdctmod(bdctimg,sj,sk,[1 1 1]);
-                tmc=tpm1(bdctimgc(:,:,7),4);
-                mahsdist(7)=mahsdistcalc(tm1,tmc,sigma);
+                %bdctimgc(:,:,7)=bdctmod(bdctimg,sj,sk,[1 1 1]);
+                tmc(:,:,7)=tmmod(bdctimg,tm3,sj,sk,[1 1 1]);
+                mahsdist(7)=mahsdistcalc(tm1,tmc(:,:,7),sigma);
             end
             
             [mahsdist_sorted,I2]=sort(mahsdist);
@@ -119,40 +119,47 @@ while n_mod<NMOD
                 end
                 switch I2(1)
                     case 1
-                        bdctimg=bdctimgc(:,:,1);
+                        bdctimg=bdctmod(bdctimg,sj,sk,[1 0 0]);
+                        tm3=tmc(:,:,1);
                         modified(sj,sk)=true;
                         n_mod=n_mod+1;
                         modflag=true;
                     case 2
-                        bdctimg=bdctimgc(:,:,2);
+                        bdctimg=bdctmod(bdctimg,sj,sk,[0 1 0]);
+                        tm3=tmc(:,:,2);
                         modified(sj,sk+1)=true;
                         n_mod=n_mod+1;
                         modflag=true;
                     case 3
-                        bdctimg=bdctimgc(:,:,3);
+                        bdctimg=bdctmod(bdctimg,sj,sk,[0 0 1]);
+                        tm3=tmc(:,:,3);
                         modified(sj,sk+2)=true;
                         n_mod=n_mod+1;
                         modflag=true;
                     case 4
-                        bdctimg=bdctimgc(:,:,4);
+                        bdctimg=bdctmod(bdctimg,sj,sk,[1 1 0]);
+                        tm3=tmc(:,:,4);
                         modified(sj,sk)=true;
                         modified(sj,sk+1)=true;
                         n_mod=n_mod+2;
                         modflag=true;
                     case 5
-                        bdctimg=bdctimgc(:,:,5);
+                        bdctimg=bdctmod(bdctimg,sj,sk,[0 1 1]);
+                        tm3=tmc(:,:,5);
                         modified(sj,sk+1)=true;
                         modified(sj,sk+2)=true;
                         n_mod=n_mod+2;
                         modflag=true;
                     case 6
-                        bdctimg=bdctimgc(:,:,6);
+                        bdctimg=bdctmod(bdctimg,sj,sk,[1 0 1]);
+                        tm3=tmc(:,:,6);
                         modified(sj,sk)=true;
                         modified(sj,sk+2)=true;
                         n_mod=n_mod+2;
                         modflag=true;
                     otherwise
-                        bdctimg=bdctimgc(:,:,7);
+                        bdctimg=bdctmod(bdctimg,sj,sk,[1 1 1]);
+                        tm3=tmc(:,:,7);
                         modified(sj,sk)=true;
                         modified(sj,sk+1)=true;
                         modified(sj,sk+2)=true;
@@ -175,7 +182,6 @@ while n_mod<NMOD
 end
 
 fprintf('%d coeff modified\n',n_mod);
-tm3=tpm1(bdctimg,4);
 ximg=bdctdec(bdctimg.*sign(bdctimg_ori));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -226,6 +232,56 @@ for i=1:3
         end
     end
 end
+
+function tm=tmmod(bdctimg,tm,sj,sk,flag)
+%modify transmition probability directly
+
+T=4;
+S=1/(128*126);
+for i=1:3
+    if flag(i)==1
+        rj=sj;
+        rk=sk+i-1;
+        if mod(bdctimg(rj,rk),2)==0
+            l=1;
+        else
+            l=-1;
+        end
+        if rk-2>0
+            y1=threshold(bdctimg(rj,rk-2)-bdctimg(rj,rk-1),T);
+            y2=threshold(bdctimg(rj,rk-1)-bdctimg(rj,rk),T);
+            y3=y1;
+            y4=threshold(bdctimg(rj,rk-1)-bdctimg(rj,rk)-l,T);
+            tm(y1+T+1,y2+T+1)=tm(y1+T+1,y2+T+1)-S;
+            tm(y3+T+1,y4+T+1)=tm(y3+T+1,y4+T+1)+S;
+        end
+        
+        if rk-1>0 && rk+1<129
+            y1=threshold(bdctimg(rj,rk-1)-bdctimg(rj,rk),T);
+            y2=threshold(bdctimg(rj,rk)-bdctimg(rj,rk+1),T);
+            y3=threshold(bdctimg(rj,rk-1)-bdctimg(rj,rk)-l,T);
+            y4=threshold(bdctimg(rj,rk)-bdctimg(rj,rk+1)+l,T);
+            tm(y1+T+1,y2+T+1)=tm(y1+T+1,y2+T+1)-S;
+            tm(y3+T+1,y4+T+1)=tm(y3+T+1,y4+T+1)+S;
+        end
+        
+        if rk+2<129
+            y1=threshold(bdctimg(rj,rk)-bdctimg(rj,rk+1),T);
+            y2=threshold(bdctimg(rj,rk+1)-bdctimg(rj,rk+2),T);
+            y3=threshold(bdctimg(rj,rk)-bdctimg(rj,rk+1)+l,T);
+            y4=y2;
+            tm(y1+T+1,y2+T+1)=tm(y1+T+1,y2+T+1)-S;
+            tm(y3+T+1,y4+T+1)=tm(y3+T+1,y4+T+1)+S;
+        end
+    end
+end
+
+function t=threshold(t,T)
+t=min(t,T);
+t=max(t,-T);
+            
+                
+                
 
 
 
