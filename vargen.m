ef9acf2d900d3f286bbe48b909f9757f8a6d14be
Luyi Chen <lychen1109@gmail.com>
2011-10-01 16:56:50 +0800

    1. no rounding; 2. switch y1 and y2

--- vargen.m
+++ vargen.m
@@ -2,11 +2,12 @@ function Z=vargen(img,T)
 %adjacent variable generation from bdctimg
 
 img=blkproc(img,[8 8],@dct2);
-img=abs(round(img));
+%img=abs(round(img));
+img=abs(img);
 Y=img(:,1:end-1)-img(:,2:end);
 Y(Y>T)=T;
 Y(Y<-T)=-T;
 Z1=Y(:,1:end-1);
 Z2=Y(:,2:end);
-Z=[Z1(:) Z2(:)];
+Z=[Z2(:) Z1(:)];
 
