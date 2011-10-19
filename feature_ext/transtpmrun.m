eb3239bc451f8b7b4720c7ed26ae91039b0200c0
Luyi Chen <lychen1109@gmail.com>
2011-10-19 17:13:23 +0800

    increase range of difference image

--- feature_ext/transtpmrun.m
+++ feature_ext/transtpmrun.m
@@ -2,7 +2,7 @@ function feat=transtpmrun(images,T)
 %run transtpm on whole dataset
 
 N=size(images,1);
-feat=zeros(2*T+1,2*T+1,N);
+feat=zeros(2*T+1,4*T+1,N);
 for i=1:N
     img=images(i,:);
     img=reshape(img,128,128);
