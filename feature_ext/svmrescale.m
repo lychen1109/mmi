84335154b037046f687298c3a67ddd8739167b5d
Luyi Chen <lychen1109@gmail.com>
2011-10-22 10:21:24 +0800

    fix elementwise logic operation

--- feature_ext/svmrescale.m
+++ feature_ext/svmrescale.m
@@ -3,7 +3,7 @@ function data_scale=svmrescale(data)
 
 maxsample=max(data);
 minsample=min(data);
-zeroidx=(maxsample==0) && (minsample==0);
+zeroidx=(maxsample==0) & (minsample==0);
 data=data(:,~zeroidx);
 %data_scale=(data - repmat(min(data,[],1),size(data,1),1)) * spdiags(1./(max(data,[],1)-min(data,[],1))',0,size(data,2),size(data,2));
 data_scale=(data-repmat(min(data,[],1),size(data,1),1))*diag(1./(max(data,[],1)-min(data,[],1)));
