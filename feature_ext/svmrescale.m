bdc841a47fe9220b14ae86d36ea2d4ee4e6694f2
Luyi Chen <lychen1109@gmail.com>
2011-11-14 18:10:45 +0800

    add range to svmrescale

--- feature_ext/svmrescale.m
+++ feature_ext/svmrescale.m
@@ -1,9 +1,13 @@
-function data_scale=svmrescale(data)
+function data_scale=svmrescale(data,range)
 %rescale data to [0 1]
 
 maxsample=max(data);
 minsample=min(data);
 zeroidx=(maxsample==0) & (minsample==0);
 data=data(:,~zeroidx);
+if isempty(range)
+    range(1,:)=max(data,[],1);
+    range(2,:)=min(data,[],1);
+end
 %data_scale=(data - repmat(min(data,[],1),size(data,1),1)) * spdiags(1./(max(data,[],1)-min(data,[],1))',0,size(data,2),size(data,2));
-data_scale=(data-repmat(min(data,[],1),size(data,1),1))*diag(1./(max(data,[],1)-min(data,[],1)));
+data_scale=(data-repmat(range(2,:),size(data,1),1))*diag(1./(range(1,:)-range(2,:)));
