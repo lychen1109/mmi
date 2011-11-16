68a5b10ae5bfb12208514685e7deb9ab1cd41e16
Luyi Chen <lychen1109@gmail.com>
2011-11-16 17:07:45 +0800

    output tp tn in svmcheck

--- svm/svmcheck.m
+++ svm/svmcheck.m
@@ -1,8 +1,8 @@
-function ac=svmcheck(label,images,range,rangec,rangeg)
+function [ac,tp,tn]=svmcheck(label,images,range,rangec,rangeg)
 %feature extraction and svm test
 
 N=size(images,1);
 tm=transmatgen(images,3);
 feat=reshape(tm,49,N)';
 feat=svmrescale(feat,range);
-ac=svmtestk(label,feat,rangec,rangeg);
+[ac,tp,tn]=svmtestk(label,feat,rangec,rangeg);
