d3751c078b2676771c93b530b00e995fb429fc3f
Luyi Chen <lychen1109@gmail.com>
2011-11-16 16:45:25 +0800

    add range c and g input

--- svm/svmcheck.m
+++ svm/svmcheck.m
@@ -1,8 +1,8 @@
-function ac=svmcheck(label,images,range)
+function ac=svmcheck(label,images,range,rangec,rangeg)
 %feature extraction and svm test
 
 N=size(images,1);
 tm=transmatgen(images,3);
 feat=reshape(tm,49,N)';
 feat=svmrescale(feat,range);
-ac=svmtestk(label,feat,0:2:10,-6:2:6);
+ac=svmtestk(label,feat,rangec,rangeg);
