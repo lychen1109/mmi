dd5b123845c27a5ae20ce0c56e297a3beb177204
Luyi Chen <lychen1109@gmail.com>
2011-11-16 16:58:57 +0800

    output predict in mysvmfun

--- svm/mysvmfun.m
+++ svm/mysvmfun.m
@@ -1,10 +1,10 @@
-function [modelstruct,accu,dvalues]=mysvmfun(labeltrain,datatrain,labeltest,datatest,theta)
+function [predict,accu,dvalues]=mysvmfun(labeltrain,datatrain,labeltest,datatest,theta)
 %this is a standard RBF kernel test function
 %C=theta(1), g=theta(2)
 
 C=2^theta(1);
 cmd=['-c ' num2str(C) ' -g ' num2str(2^theta(2))];
 model=svmtrain(labeltrain,datatrain,cmd);
-[~,accu,dvalues]=svmpredict(labeltest,datatest,model);
-modelstruct=modelparse(model,C);
+[predict,accu,dvalues]=svmpredict(labeltest,datatest,model);
+%modelstruct=modelparse(model,C);
 
