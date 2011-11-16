55bcd96cc42c71d8174aaaaea2730609bec292ed
Luyi Chen <lychen1109@gmail.com>
2011-11-16 16:20:38 +0800

    change to run parameter selection in every training set

--- svm/svmtestk.m
+++ svm/svmtestk.m
@@ -1,14 +1,20 @@
-function ac=svmtestk(label,data,cvpk,theta)
+function ac=svmtestk(label,data,rangec,rangeg)
 %test accuracy of a set of splits
 
 ac=zeros(1,5);
+cvpk=cvpartition(label,'kfold',5);
 
 for i=1:5    
     dataTrain=data(cvpk.training(i),:);
     labelTrain=label(cvpk.training(i));
     dataTest=data(cvpk.test(i),:);
     labelTest=label(cvpk.test(i));
+    thetas=svmgrid2(labelTrain,dataTrain,rangec,rangeg);
+    theta=median(thetas);
+    fprintf('parameter used for split %d: (%d,%d)\n',i,theta);
     [~,accu]=mysvmfun(labelTrain,dataTrain,labelTest,dataTest,theta);
     ac(i)=accu(1);
     fprintf('accuracy of set %d is %g\n',i,ac(i));
 end
+
+ac=mean(ac);
