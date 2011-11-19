0a598f4382290a07f86e1ba031f42e5e5981e80a
Luyi Chen <lychen1109@gmail.com>
2011-11-19 11:43:36 +0800

    update input parameters

--- checkdvalues.m
+++ checkdvalues.m
@@ -1,11 +1,9 @@
-function dvalues=checkdvalues(model,range,data)
+function dvalues=checkdvalues(model,range,images)
 %check dvalues of validation set under the trained model
 
-images=data.datavalidate;
-label=data.labelvalidate;
 N=size(images,1);
 tm=transmatgen(images,3);
 feat=reshape(tm,49,N)';
 feat=svmrescale(feat,range);
-[~,~,dvalues]=svmpredict(label,feat,model);
+[~,~,dvalues]=svmpredict(zeros(N,1),feat,model);
 
