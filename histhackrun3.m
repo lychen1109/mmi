6e84c7e364c6c0431d5e11bf35e7221128b3ee3c
Luyi Chen <lychen1109@gmail.com>
2011-11-27 18:34:44 +0800

    change histhackrun3 to use histhacksvm2

--- histhackrun3.m
+++ histhackrun3.m
@@ -1,5 +1,5 @@
-function [datavalidate,modified,tm,output]=histhackrun3(data,model,range)
-%batch run of histhackm2step, using parfor
+function [ximages,returntypes]=histhackrun3(images,samplevalues,model,range)
+%batch run of histhack* function, using parfor
 
 nworker=matlabpool('size');
 if nworker<2
@@ -8,33 +8,20 @@ else
     fprintf('Running on %d workers\n',nworker);
 end
 
-datavalidate=data.datavalidate;
-labelvalidate=data.labelvalidate;
-
-ximages=datavalidate(labelvalidate==0,:);
-N=size(ximages,1);
-modified=false(N,128^2);
-dout_ori=zeros(N,1);
-moddout=zeros(N,1);
-n_mod=zeros(N,1);
-tm=zeros(N,49);
+N=size(images,1);
+ximages=zeros(size(images));
+returntypes=zeros(N,1);
 
 parfor i=1:N
     fprintf('processing image %d\n',i);
-    simg=ximages(i,:);
+    simg=images(i,:);
     simg=reshape(simg,128,128);    
-    [ximg,modifiedtmp,tmtmp,outputtmp]=histhacksvmstep(simg,model,range);
-    ximages(i,:)=ximg(:)';
-    modified(i,:)=modifiedtmp(:)';
-    tm(i,:)=tmtmp(:)';
-    dout_ori(i)=outputtmp.dout_ori;
-    moddout(i)=outputtmp.moddout;
-    n_mod(i)=outputtmp.n_mod;
+    [ximg,~,returntypes(i)]=histhacksvm2(simg,samplevalues(i),model,range);
+    ximages(i,:)=ximg(:)';    
 end
-datavalidate(labelvalidate==0,:)=ximages;
-output.dout_ori=dout_ori;
-output.moddout=moddout;
-output.n_mod=n_mod;
+
+save histhacksvm2result ximages returntypes
+
 
 
 
