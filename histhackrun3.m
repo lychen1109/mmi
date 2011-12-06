12967b2af193edf11727b1dd80926e6041b0753c
Luyi Chen <lychen1109@gmail.com>
2011-12-06 17:50:43 +0800

    add record output

--- histhackrun3.m
+++ histhackrun3.m
@@ -1,4 +1,4 @@
-function [ximages]=histhackrun3(images,timages)
+function [ximages,record]=histhackrun3(images,timages)
 %batch run of histhack* function, using parfor
 
 nworker=matlabpool('size');
@@ -10,6 +10,7 @@ end
 
 N=size(images,1);
 ximages=zeros(size(images));
+record=cell(N,1);
 
 parfor i=1:N
     fprintf('processing image %d\n',i);
@@ -17,7 +18,7 @@ parfor i=1:N
     simg=reshape(simg,128,128);
     aimg=timages(i,:);
     aimg=reshape(aimg,128,128);
-    [ximg]=histhacknew(simg,aimg);
+    [ximg,record{i}]=histhacknew(simg,aimg);
     ximages(i,:)=ximg(:)';    
 end
 
