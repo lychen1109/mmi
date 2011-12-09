7fa474a93f8edf8acaaa9c1d5c298259c5bbcccf
Luyi Chen <lychen1109@gmail.com>
2011-12-09 17:14:35 +0800

    new flagcreationl2

--- flagcreationl2.m
+++ flagcreationl2.m
@@ -1,15 +1,7 @@
-function output=flagcreationl2(img,tmtarget,N)
+function [flag,moddist,mindist]=flagcreationl2(bdctimg,tmtarget,coefloc)
 %create location and flags that can improve image and target distance
 
-coefidx=randperm(128^2-256);
-coefidx=coefidx(1:N);
-flags=zeros(N,3);
-moddist=zeros(N,1);
-bingrads=zeros(N,1);
 T=3;
-
-bdctimg=blkproc(img,[8 8],@dct2);
-bdctimg=abs(round(bdctimg));
 tm=tpm1(bdctimg,T);
 dist_ori=norm(tm(:)-tmtarget(:));
 
@@ -27,29 +19,20 @@ for i=-1:1
     end
 end
 
-for i=1:N
-    [sj,sk]=ind2sub(size(bdctimg),coefidx(i));
-    y1=threshold(bdctimg(sj,sk)-bdctimg(sj,sk+1),T)+T+1;
-    y2=threshold(bdctimg(sj,sk+1)-bdctimg(sj,sk+2),T)+T+1;
-    binidx=sub2ind(size(tm),y1,y2);
-    grad=(tm(binidx)-tmtarget(binidx))/dist_ori;
-    bingrads(i)=grad;
-    dist=zeros(1,26);
-    dist(1:26)=inf;
-    for j=1:26
-        if ~any(bdctimg(sj,sk+(0:2))+flagstr(j,:)<0)
-            tmnew=tmmod(bdctimg,tm,sj,sk,flagstr(j,:),T);
-            dist(j)=norm(tmnew(:)-tmtarget(:));
-        end
+[sj,sk]=ind2sub(size(bdctimg),coefloc);
+dist=zeros(1,26);
+dist(1:26)=inf;
+for j=1:26
+    if ~any(bdctimg(sj,sk+(0:2))+flagstr(j,:)<0)
+        tmnew=tmmod(bdctimg,tm,sj,sk,flagstr(j,:),T);
+        dist(j)=norm(tmnew(:)-tmtarget(:));
     end
-    [mindist,mindistidx]=min(dist);
-    flags(i,:)=flagstr(mindistidx,:);
-    moddist(i)=mindist-dist_ori;    
 end
+[mindist,mindistidx]=min(dist);
+flag=flagstr(mindistidx,:);
+moddist=mindist-dist_ori;
 
-output.coefidx=coefidx;
-output.flags=flags;
-output.moddist=moddist;
-output.bingrads=bingrads;
+
+
 
     
