21873d787a4b7d9a2026f3f0db9e350cf0cf4692
Luyi Chen <lychen1109@gmail.com>
2011-12-09 17:01:35 +0800

    new histhacknew

--- histhacknew.m
+++ histhacknew.m
@@ -12,46 +12,36 @@ bdctimg=round(bdctimg);
 bdctsign=sign(bdctimg);
 bdctsign(bdctsign==0)=1;
 bdctimg=abs(bdctimg);
-N=1000;
-nbatch=10;%number of bdct coeff used in one iteration
 tm=tpm1(bdctimg,T);
-dist_ori=norm(tm(:)-tmtarget(:));
-distrec=dist_ori;%record dist in every iteration
+
+dist_current=norm(tm(:)-tmtarget(:));
+distrec=dist_current;%record dist in every iteration
 iter=0;
 fprintf('iter %d: %g\n',iter,distrec(end));
-notfinish=true;
 
-while notfinish      
-    flagstruct=flagcreationl2(img,tmtarget,N);
-    coefidx=flagstruct.coefidx;
-    flags=flagstruct.flags;
-    moddist=flagstruct.moddist;
-    coefidx=coefidx(moddist<0);
-    flags=flags(moddist<0);
-    moddist=moddist(moddist<0);
-    M=size(flags,1);
-    
-    [~,sortedidx]=sort(moddist,1,'ascend');
-    bdctmod=zeros(size(img));
-    dist_current=distrec(end);
-    modidx=1;
-    while dist_current>=distrec(end) && modidx+nbatch-1<=M
-        for i=modidx:modidx+nbatch-1
-            [sj,sk]=ind2sub(size(img),coefidx(sortedidx(i)));
-            bdctmod(sj,sk:sk+2)=bdctmod(sj,sk:sk+2)+flags(sortedidx(i),:).*bdctsign(sj,sk:sk+2);
-        end
-        imgmod=blkproc(bdctmod,[8 8],@idct2);
-        imgmod=round(imgmod);
-        dist_current=distcalc(img+imgmod,tmtarget);
-        modidx=modidx+nbatch;
-    end    
-    
-    if dist_current<distrec(end)        
-        distrec=cat(1,distrec,dist_current);
-        img=img+imgmod;
-        iter=iter+1;
-        fprintf('iter %d: %g\n',iter,dist_current);        
-    else
-        notfinish=false;        
+while iter<1000 && dist_current>1e-4
+    coefloc=randperm(128^2-256);
+    coefidx=1;
+    [flag,moddist,mindist]=flagcreationl2(bdctimg,tmtarget,coefloc(coefidx));
+    while moddist>=0 && coefidx<=128^2-256
+        coefidx=coefidx+1;
+        [flag,moddist,mindist]=flagcreationl2(bdctimg,tmtarget,coefloc(coefidx));
+    end
+    if moddist>=0
+        break;
+    end
+    dist_current=mindist;
+    [sj,sk]=ind2sub(size(bdctimg),coefloc(coefidx));
+    bdctimg(sj,sk:sk+2)=bdctimg(sj,sk:sk+2)+flag;
+    iter=iter+1;     
+    distrec=cat(1,distrec,dist_current);    
+    fprintf('iter %d: %g\n',iter,dist_current);
+    if mod(iter,20)==0
+        img=bdctdec(bdctimg.*bdctsign);
+        bdctimg=blkproc(img,[8 8],@dct2);
+        bdctimg=round(bdctimg);
+        bdctsign=sign(bdctimg);
+        bdctsign(bdctsign==0)=1;
+        bdctimg=abs(bdctimg);
     end        
 end
