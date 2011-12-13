aeb02d2a62705c93bfc504e97b027a726f906f0e
Luyi Chen <lychen1109@gmail.com>
2011-12-13 16:02:56 +0800

    new svmtest

--- svm/svmtest.m
+++ svm/svmtest.m
@@ -1,41 +1,26 @@
-function [ac,nSV]=svmtest(class,data,cvpa)
+function [ac,tp,tn]=svmtest(class,data,cvpa)
 %test the svm accuracy with k split
 
 k=length(cvpa);
 ac=zeros(k,1);
-nSV=zeros(k,1);
-thetas=zeros(5,2);
+tp=zeros(k,1);
+tn=zeros(k,1);
 
-for i=1:5    
+for i=1:k    
     CVP=cvpa(i);
     dataTrain=data(CVP.training,:);    
     grpTrain=class(CVP.training);
     dataTest=data(CVP.test,:);
     grpTest=class(CVP.test);
-    thetas(i,:)=svmgrid(grpTrain,dataTrain,12:-1:0,-6:6);
-    [modelstruct,accu,~]=mysvmfun(grpTrain,dataTrain,grpTest,dataTest,thetas(i,:));
-    nSV(i)=size(modelstruct.SVs,1);    
-    Nc=length(modelstruct.Yc);    
-    ac(i)=accu(1);
-    fprintf('accuracy %g with nSV %d,bSV %d\n',ac(i),nSV(i),Nc);
+    thetas=svmgrid2(grpTrain,dataTrain,0:2:12,-6:2:6);
+    [predict]=mysvmfun(grpTrain,dataTrain,grpTest,dataTest,thetas(1,:));      
+    ac(i)=sum(predict==grpTest)/length(grpTest);
+    tp(i)=sum(grpTest==0 & predict==grpTest)/sum(grpTest==0);
+    tn(i)=sum(grpTest==1 & predict==grpTest)/sum(grpTest==1);
+    fprintf('accuracy %g tp %g tn %g\n',ac(i),tp(i),tn(i));
 end
 
-theta=median(thetas);
-fprintf('thetas selected for the rests: C=%d, g=%d\n',theta(1),theta(2));
-for i=6:k
-    CVP=cvpa(i);
-    dataTrain=data(CVP.training,:);    
-    grpTrain=class(CVP.training);
-    dataTest=data(CVP.test,:);
-    grpTest=class(CVP.test);    
-    [modelstruct,accu,~]=mysvmfun(grpTrain,dataTrain,grpTest,dataTest,theta);
-    nSV(i)=size(modelstruct.SVs,1);    
-    Nc=length(modelstruct.Yc);    
-    ac(i)=accu(1);
-    fprintf('accuracy %g with nSV %d,bSV %d\n',ac(i),nSV(i),Nc);
-end
-
-fprintf('mean accuracy:%g (%g), avg nSV:%g (%g)\n',mean(ac),std(ac),mean(nSV),std(nSV));
+fprintf('final accuracy: %g (%g), tp: %g (%g), tn:%g (%g)\n',mean(ac),std(ac),mean(tp),std(tp),mean(tn),std(tn));
 
 
     
