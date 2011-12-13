bb0d737ad65ec10987f73bbe16873f9bcc05e362
Luyi Chen <lychen1109@gmail.com>
2011-12-13 16:05:46 +0800

    new dataspliter using cell

--- util/dataspliter.m
+++ util/dataspliter.m
@@ -1,8 +1,8 @@
-function cvparray=dataspliter(N,label,n_test)
+function cvps=dataspliter(N,label,n_test)
 %randomly generate split sets
 
-cvparray(1:N)=cvpartition(label,'holdout',n_test);
-for i=2:N
-    cvparray(i)=cvpartition(label,'holdout',n_test);
+cvps=cell(N,1);
+for i=1:N
+    cvps{i}=cvpartition(label,'holdout',n_test);
 end
     
