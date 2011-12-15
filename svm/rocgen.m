9ba655375c432a7e1b5e2b43a813db11cdb1eff5
Luyi Chen <lychen1109@gmail.com>
2011-12-15 11:34:04 +0800

    bugfix

--- svm/rocgen.m
+++ svm/rocgen.m
@@ -1,16 +1,9 @@
-function rocgen(label,feat,cvp,threshold)
+function rocgen(label,dvalues,threshold)
 %generate roc curve
 
-labeltrain=label(cvp.training);
-datatrain=feat(cvp.training,:);
-cvp5=cvpartition(labeltrain,'kfold',5);
-dvalues=zeros(size(labeltrain));
-for i=1:5    
-    [~,~,dvalues(cvp5.test(i))]=mysvmfun(labeltrain(cvp5.training(i)),datatrain(cvp5.training(i),:),labeltrain(cvp5.test(i)),datatrain(cvp5.test(i),:),[8 -6]);
-end
 predict=ones(size(dvalues));
 predict(dvalues<threshold)=0;
-ac=sum(predict==labeltrain)/length(labeltrain);
-tp=sum(labeltrain==0 & predict==labeltrain)/sum(labeltrain==0);
-fp=sum(labeltrain==1 & predict==0)/sum(labeltrain==1);
+ac=sum(predict==label)/length(label);
+tp=sum(label==0 & predict==0)/sum(label==0);
+fp=sum(label==1 & predict==0)/sum(label==1);
 fprintf('accuracy=%g, tp=%g, fp=%g\n',ac,tp,fp);
