f083580963bba6e2a6ecbe351709beac059a4a79
Luyi Chen <lychen1109@gmail.com>
2011-12-27 09:38:59 +0800

    margin hist creation

--- feature_ext/imghist.m
+++
@@ -1,8 +0,0 @@
-function h=imghist(img,T)
-%image histogram
-
-[N,M]=size(img);
-img(img>T)=T;
-x=0:T;
-h=hist(img(:),x);
-h=h/(N*M);
---
+++ feature_ext/marginhist.m
@@ -0,0 +1,14 @@
+function h=marginhist(img)
+%image histogram
+
+bdctimg=blkproc(img,[8 8],@dct2);
+bdctimg=abs(round(bdctimg));
+diff=bdctimg(:,1:end-1)-bdctimg(:,2:end);
+T=8;
+[N,M]=size(img);
+diff(diff>T)=T;
+diff(diff<-T)=-T;
+x=-T:T;
+h=hist(diff(:),x);
+h=h/(N*(M-1));
+h=h(:)';
