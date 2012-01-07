05ede02218b7f6ffd82ee340d6e3b89b4fa5bc7d
Luyi Chen <lychen1109@gmail.com>
2012-01-07 16:17:10 +0800

    change output format of AIC

--- gmm/gmmfit.m
+++ gmm/gmmfit.m
@@ -2,15 +2,16 @@ function gm=gmmfit(X,rangek)
 %fit pdf of X with the best GMM
 
 AICs=zeros(size(rangek));
-opt=statset('display','final','maxiter',5000);
+opt=statset('display','final','maxiter',1000);
 K=length(rangek);
 gms=cell(K,1);
 for i=1:K
     k=rangek(i);
     gms{i}=gmdistribution.fit(X,k,'replicates',20,'CovType','diagonal','SharedCov',false,'options',opt);
-    AICs(i)=gms{i}.AIC;
-    fprintf('AIC of k=%d is %g\n',k,AICs(i));
+    AICs(i)=gms{i}.AIC;    
 end
 
+fprintf('AIC of k=%d is %g\n',[rangek;AICs]);
 [~,minAIC]=min(AICs);
+fprintf('number of component is %d\n',rangek(minAIC));
 gm=gms{minAIC};
