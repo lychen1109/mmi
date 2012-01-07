5c5bc421da494ad3a86c8319e97f3949dee24254
Luyi Chen <lychen1109@gmail.com>
2012-01-07 15:01:30 +0800

    change tpm1 to use N-1 rows in image

--- feature_ext/tpm1.m
+++ feature_ext/tpm1.m
@@ -2,7 +2,10 @@ function D=tpm1(img,T)
 %tmp horizontal with threshold
 
 [N,M]=size(img);
-diffimg=img(:,1:M-1)-img(:,2:M);
+img=blkproc(img,[8 8],@dct2);
+img=abs(round(img));
+diffimg=img(1:N-1,1:M-1)-img(1:N-1,2:M);
+%diffimg=round(diffimg);
 
 diffimg(diffimg>T)=T;
 diffimg(diffimg<-T)=-T;
@@ -10,12 +13,12 @@ diffimg=diffimg+T+1;
 msize=2*T+1;
 
 D=zeros(msize,msize);
-for i=1:N
+for i=1:N-1
     for j=1:M-2
        D(diffimg(i,j),diffimg(i,j+1))=D(diffimg(i,j),diffimg(i,j+1))+1;
     end
 end
-D=D/(N*(M-2));
+D=D/((N-1)*(M-2));
 
 
 
