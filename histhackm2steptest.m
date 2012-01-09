ee88c08e27708520329b5d2eca414296f2c51be0
Luyi Chen <lychen1109@gmail.com>
2012-01-09 10:53:06 +0800

    del file

--- histhackm2steptest.m
+++
@@ -1,7 +0,0 @@
-function [ximg,modified,output]=histhackm2steptest(data,gm1,gm2)
-%test histhackm2steptest
-
-simg=data.datavalidate(1,:);
-simg=reshape(simg,128,128);
-modified=false(size(simg));
-[ximg,modified,output]=histhackm2step(simg,modified,gm1,gm2);
