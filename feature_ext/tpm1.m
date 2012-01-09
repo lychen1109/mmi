c14f0f62996fcd2bab76c8b3af74762c853c7288
Luyi Chen <lychen1109@gmail.com>
2012-01-09 11:30:44 +0800

    comment out bdct and rounding part

--- feature_ext/tpm1.m
+++ feature_ext/tpm1.m
@@ -2,8 +2,8 @@ function D=tpm1(img,T)
 %tmp horizontal with threshold
 
 [N,M]=size(img);
-img=blkproc(img,[8 8],@dct2);
-img=abs(round(img));
+% img=blkproc(img,[8 8],@dct2);
+% img=abs(round(img));
 diffimg=img(1:N-1,1:M-1)-img(1:N-1,2:M);
 %diffimg=round(diffimg);
 
