146640a8a178ca01ded66a1d812a6253d5259d79
Luyi Chen <lychen1109@gmail.com>
2012-01-12 16:19:07 +0800

    new improved greedy algorithm

--- feature_ext/tmmod2.m
+++ feature_ext/tmmod2.m
@@ -1,28 +1,35 @@
-function tm=tmmod2(diffimg,tm,sj,sk,flag,T)
+function out=tmmod2(bdctimg,tm,sj,sk,flag,T)
 %tmmod used for diffimg
 
 S=1/127/126;
-if flag(1)~=0 && sk-1>0
-    y1=threshold(diffimg(sj,sk-1),T);
-    y2=threshold(diffimg(sj,sk),T);
+tmold=tm;
+if sk-2>0
+    y1=threshold(bdctimg(sj,sk-2)-bdctimg(sj,sk-1,T));
+    y2=threshold(bdctimg(sj,sk-1)-bdctimg(sj,sk),T);
     y3=y1;
-    y4=threshold(diffimg(sj,sk)+flag(1),T);
+    y4=threshold(bdctimg(sj,sk-1)-bdctimg(sj,sk)-flag,T);
     tm(y1+T+1,y2+T+1)=tm(y1+T+1,y2+T+1)-S;
     tm(y3+T+1,y4+T+1)=tm(y3+T+1,y4+T+1)+S;
 end
 
-if flag(2)~=0 && sk+2<128
-    y1=threshold(diffimg(sj,sk+1),T);
-    y2=threshold(diffimg(sj,sk+2),T);
-    y3=threshold(diffimg(sj,sk+1)+flag(2),T);
+if sk+2<129
+    y1=threshold(bdctimg(sj,sk)-bdctimg(sj,sk+1),T);
+    y2=threshold(bdctimg(sj,sk+1)-bdctimg(sj,sk+2),T);
+    y3=threshold(bdctimg(sj,sk)-bdctimg(sj,sk+1)+flag,T);
     y4=y2;
     tm(y1+T+1,y2+T+1)=tm(y1+T+1,y2+T+1)-S;
     tm(y3+T+1,y4+T+1)=tm(y3+T+1,y4+T+1)+S;
 end
 
-y1=threshold(diffimg(sj,sk),T);
-y2=threshold(diffimg(sj,sk+1),T);
-y3=threshold(diffimg(sj,sk)+flag(1),T);
-y4=threshold(diffimg(sj,sk+1)+flag(2),T);
+y1=threshold(bdctimg(sj,sk-1)-bdctimg(sj,sk),T);
+y2=threshold(bdctimg(sj,sk)-bdctimg(sj,sk+1),T);
+y3=threshold(bdctimg(sj,sk-1)-bdctimg(sj,sk)-flag,T);
+y4=threshold(bdctimg(sj,sk)-bdctimg(sj,sk+1)+flag,T);
 tm(y1+T+1,y2+T+1)=tm(y1+T+1,y2+T+1)-S;
 tm(y3+T+1,y4+T+1)=tm(y3+T+1,y4+T+1)+S;
+out.tm=tm;
+if ~isequal(tm,tmold)
+    out.changed=true;
+else
+    out.changed=false;
+end
--- histhackm.m
+++ histhackm.m
@@ -4,75 +4,56 @@ function [bdctimg,delta,logpdfori,logpdfnew]=histhackm(simg,gm,PCAstruct)
 
 T=3;
 bdctimg=blkproc(simg,[8 8],@dct2);
+bdctimg=round(bdctimg);
 bdctimgsign=sign(bdctimg);
 bdctimgsign(bdctimgsign==0)=1;
-bdctimg=round(abs(bdctimg));
-[N,M]=size(bdctimg);
-diffimg=bdctimg(1:N-1,1:M-1)-bdctimg(1:N-1,2:M);
-diffimgori=diffimg;
-tm=tpmf(diffimg,T);
-tmnorm=rownorm(tm);
-logpdfori=log10(tmpdfcalc(gm,tmnorm,PCAstruct));
+bdctimgori=bdctimg;
+bdctimg=abs(bdctimg);
+tm=tpm1(bdctimg,T);
+logpdfori=log10(tmpdfcalc(gm,tm,PCAstruct));
 fprintf('logpdfori=%g\n',logpdfori);
 logpdfnew=logpdfori;
 
-while true
-    randidx=randperm(127*126);
-    loc_idx=1; %start from the first location
-    outstruct=flaggen(diffimg,diffimgori,randidx(loc_idx),bdctimg,PCAstruct,logpdfnew,gm,tm);
-    while outstruct.modflag==false && loc_idx<127*126
-        loc_idx=loc_idx+1;
-        outstruct=flaggen(diffimg,diffimgori,randidx(loc_idx),bdctimg,PCAstruct,logpdfnew,gm,tm);
-    end
-    if outstruct.modflag
-        [sj,sk]=ind2sub(size(diffimg),randidx(loc_idx));
-        diffimg(sj,sk:sk+1)=diffimg(sj,sk:sk+1)+outstruct.flag;        
-        logpdfnew=outstruct.logpdfnew;
-        fprintf('logpdfnew=%g\n',logpdfnew);
-    else
-        break
+for sj=1:127
+    for sk=1:128
+        outstruct=flaggen(bdctimg,sj,sk,PCAstruct,logpdfnew,gm,tm);
+        if outstruct.modflag
+            bdctimg(sj,sk)=bdctimg(sj,sk)+outstruct.flag;
+            logpdfnew=outstruct.logpdfnew;
+            fprintf('logpdfnew=%g\n',logpdfnew);
+        end
     end
 end
-delta=diffimgori-diffimg;
-bdctimg(1:end-1,1:end-1)=bdctimg(1:end-1,2:end)+diffimg;
+
 bdctimg=bdctimg.*bdctimgsign;
+delta=bdctimgori-bdctimg;
 
 function pdfvalue=tmpdfcalc(gm,tm,PCAstruct)
 %calculate pdf of gmm in PCA space
+tm=rownorm(tm);
 coeff=PCAstruct.coeff;
 means=PCAstruct.means;
 score=(tm(1:42)-means)*coeff;
 pdfvalue=pdf(gm,score);
        
-function output=flaggen(diffimg,diffimgori,loc,bdctimg,PCAstruct,logpdfori,gm,tm)
+function output=flaggen(bdctimg,sj,sk,PCAstruct,logpdfori,gm,tm)
 %check if logpdf can be improved
 output.modflag=false;
 logpdfnew=logpdfori;
-[sj,sk]=ind2sub(size(diffimg),loc);
-bdctimgp=bdctimg(1:end-1,2:end);
-for i=-2:2
-    for j=-2:2
-        if i==0 && j==0
-            continue;
-        end
-        diffimgnew=diffimg;
-        diffimgnew(sj,sk:sk+1)=diffimgnew(sj,sk:sk+1)+[i,j];
-        if any(bdctimgp(sj,sk:sk+1)+diffimgnew(sj,sk:sk+1)<0)
-            continue;
-        end
-        if max(max(abs(diffimgnew-diffimgori)))>2
-            continue;
-        end    
-        %tmnew=tpmf(diffimgnew,3);
-        tmnew=tmmod2(diffimgnew,tm,sj,sk,[i j],3);
-        tmnorm=rownorm(tmnew);
-        logpdf=log10(tmpdfcalc(gm,tmnorm,PCAstruct));
-        if logpdf>logpdfnew
-            output.modflag=true;
-            logpdfnew=logpdf;
-            output.flag=[i,j];
-            output.logpdfnew=logpdfnew;
-        end
+for i=max(-2,-bdctimg(sj,sk)):2
+    if i==0
+        continue;
+    end
+    out=tmmod2(bdctimg,tm,sj,sk,i,3);
+    if ~out.changed
+        continue;
+    end
+    logpdf=log10(tmpdfcalc(gm,out.tm,PCAstruct));
+    if logpdf>logpdfnew
+        output.modflag=true;
+        logpdfnew=logpdf;
+        output.flag=i;
+        output.logpdfnew=logpdfnew;
     end
 end
 
