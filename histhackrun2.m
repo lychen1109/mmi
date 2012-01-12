443166f78a128b1456f6aec0f55f9975e0d8784d
Luyi Chen <lychen1109@gmail.com>
2012-01-12 17:23:09 +0800

    change histhackrun2 to support new histhackm

--- histhackrun2.m
+++ histhackrun2.m
@@ -1,25 +1,12 @@
-function [ximages,outputs]=histhackrun2(spfilenames,gm1,gm2,varargin)
+function [ximages]=histhackrun2(images,gm,PCAstruct)
 %batch run of histhackm, using parfor
 
-root='C:\data\ImSpliceDataset\';
-nvar=length(varargin);
-for i=1:nvar/2
-    switch varargin{2*i-1}
-        case 'root'
-            root=varargin{2*i};
-    end
-end
-nworker=matlabpool('size');
-if nworker<2
-    error('Increase the performance by launching matlabpool\n');
-else
-    fprintf('Running on %d workers\n',nworker);
-end
-
-N=size(spfilenames,1);
+N=size(images,1);
 ximages=zeros(N,128^2);
 parfor i=1:N    
     fprintf('processing image %d\n',i);
-    [ximg,outputs(i)]=histhackm2(spfilenames{i},gm1,gm2,'root',root);    
+    img=images(i,:);
+    img=reshape(img,128,128);
+    [ximg]=histhackm(img,gm,PCAstruct);    
     ximages(i,:)=ximg(:)';    
 end
