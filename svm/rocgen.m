8c5e65a6c99a25fc0c5dd67589386198c14d4616
Luyi Chen <lychen1109@gmail.com>
2012-01-13 16:17:22 +0800

    change output of rocgen

--- svm/rocgen.m
+++ svm/rocgen.m
@@ -1,9 +1,9 @@
-function rocgen(label,dvalues,threshold)
+function [tp,fp]=rocgen(label,dvalues,threshold)
 %generate roc curve
 
 predict=ones(size(dvalues));
 predict(dvalues<threshold)=0;
-ac=sum(predict==label)/length(label);
+%ac=sum(predict==label)/length(label);
 tp=sum(label==0 & predict==0)/sum(label==0);
 fp=sum(label==1 & predict==0)/sum(label==1);
-fprintf('accuracy=%g, tp=%g, fp=%g\n',ac,tp,fp);
+%fprintf('accuracy=%g, tp=%g, fp=%g\n',ac,tp,fp);
