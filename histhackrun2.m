964d6c4ad672618138c4ea3ff262375e04d01d3c
Luyi Chen <lychen1109@gmail.com>
2012-01-18 15:14:05 +0800

    modified histhackrun2 for the second run

--- histhackrun2.m
+++ histhackrun2.m
@@ -1,9 +1,8 @@
-function output=histhackrun2(images,gm,PCAstruct)
+function output=histhackrun2(images,gm,PCAstruct,target,randidxs)
 %batch run of histhackm, using parfor
 
 N=size(images,1);
 ximages=zeros(N,128^2);
-randidxs=zeros(N,127*128);
 deltas=zeros(N,128^2);
 logpdfori=zeros(N,1);
 logpdfnew=zeros(N,1);
@@ -11,9 +10,7 @@ parfor i=1:N
     fprintf('processing image %d\n',i);
     img=images(i,:);
     img=reshape(img,128,128);
-    randidx=randperm(127*128);
-    randidxs(i,:)=randidx;
-    [ximg,delta,logpdfori(i),logpdfnew(i)]=histhackm(img,gm,PCAstruct,randidx,1,inf);    
+    [ximg,delta,logpdfori(i),logpdfnew(i)]=histhackm(img,gm,PCAstruct,randidxs(i,:),1,target(i));    
     ximages(i,:)=ximg(:)';
     deltas(i,:)=delta(:)';
 end
