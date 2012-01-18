d0233f496c8e7e7527afb391793d4b1d16021d8b
Luyi Chen <lychen1109@gmail.com>
2012-01-18 11:15:40 +0800

    fix tm bug

--- histhackm.m
+++ histhackm.m
@@ -1,4 +1,4 @@
-function [bdctimg,delta,logpdfori,logpdfnew]=histhackm(simg,gm,PCAstruct)
+function [bdctimg,delta,logpdfori,logpdfnew]=histhackm(simg,gm,PCAstruct,randidx,K,target)
 %reshape histogram by modifying BDCT coeff
 %target histogram is modeled with GMM
 
@@ -13,15 +13,19 @@ tm=tpm1(bdctimg,T);
 logpdfori=log10(tmpdfcalc(gm,tm,PCAstruct));
 fprintf('logpdfori=%g\n',logpdfori);
 logpdfnew=logpdfori;
-
-for sj=1:127
-    for sk=1:128
-        outstruct=flaggen(bdctimg,sj,sk,PCAstruct,logpdfnew,gm,tm);
-        if outstruct.modflag
-            bdctimg(sj,sk)=bdctimg(sj,sk)+outstruct.flag;
-            logpdfnew=outstruct.logpdfnew;
-            fprintf('logpdfnew=%g\n',logpdfnew);
-        end
+for i=1:127*128
+    if logpdfnew>=target
+        break;
+    end
+    [sj,sk]=ind2sub([127 128],randidx(i));
+    outstruct=flaggen(bdctimg,sj,sk,PCAstruct,logpdfnew,gm,tm,K);
+    if outstruct.modflag
+        bdctimg(sj,sk)=bdctimg(sj,sk)+outstruct.flag;
+        tm=outstruct.tm;
+        logpdfnew=outstruct.logpdfnew;
+    end
+    if mod(i,1000)==0
+        fprintf('i=%d, logpdfnew=%g\n',i,logpdfnew);
     end
 end
 
@@ -36,11 +40,11 @@ means=PCAstruct.means;
 score=(tm(1:42)-means)*coeff;
 pdfvalue=pdf(gm,score);
        
-function output=flaggen(bdctimg,sj,sk,PCAstruct,logpdfori,gm,tm)
+function output=flaggen(bdctimg,sj,sk,PCAstruct,logpdfori,gm,tm,K)
 %check if logpdf can be improved
 output.modflag=false;
 logpdfnew=logpdfori;
-for i=max(-2,-bdctimg(sj,sk)):2
+for i=max(-K,-bdctimg(sj,sk)):K
     if i==0
         continue;
     end
@@ -54,6 +58,7 @@ for i=max(-2,-bdctimg(sj,sk)):2
         logpdfnew=logpdf;
         output.flag=i;
         output.logpdfnew=logpdfnew;
+        output.tm=out.tm;
     end
 end
 
