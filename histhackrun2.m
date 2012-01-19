3c2c7e1e9df42d076bfd67157be7d5d5f417f6e3
Luyi Chen <lychen1109@gmail.com>
2012-01-19 18:26:15 +0800

    modified histhackrun2 for new histhack3

--- histhackrun2.m
+++ histhackrun2.m
@@ -1,23 +1,28 @@
-function output=histhackrun2(images,gm,PCAstruct,target,randidxs)
-%batch run of histhackm, using parfor
+function output=histhackrun2(images,targets)
+%batch run of histhack3, using parfor
 
 N=size(images,1);
+M=size(targets,1);
+randidx=randperm(M);
+targets=targets(randidx(1:N),:);
+
 ximages=zeros(N,128^2);
 deltas=zeros(N,128^2);
-logpdfori=zeros(N,1);
-logpdfnew=zeros(N,1);
+dist_ori=zeros(N,1);
+dist=zeros(N,1);
 parfor i=1:N    
     fprintf('processing image %d\n',i);
     img=images(i,:);
     img=reshape(img,128,128);
-    [ximg,delta,logpdfori(i),logpdfnew(i)]=histhackm(img,gm,PCAstruct,randidxs(i,:),1,target(i));    
+    target=targets(i,:);
+    target=reshape(target,128,128);
+    [ximg,delta,dist_ori(i),dist(i)]=histhack3(img,target);    
     ximages(i,:)=ximg(:)';
     deltas(i,:)=delta(:)';
 end
 
 output.bdctimgs=ximages;
-output.randidxs=randidxs;
 output.deltas=deltas;
-output.logpdfori=logpdfori;
-output.logpdfnew=logpdfnew;
+output.dist_ori=dist_ori;
+output.dist=dist;
 
