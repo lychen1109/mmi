99ce1932fa3fb1de57e19c968cde595afb4195a3
Luyi Chen <lychen1109@gmail.com>
2012-02-04 14:34:52 +0800

    modified histhackrun2

--- histhackrun2.m
+++ histhackrun2.m
@@ -2,10 +2,6 @@ function output=histhackrun2(images,targets)
 %batch run of histhack3, using parfor
 
 N=size(images,1);
-M=size(targets,1);
-randidx=randperm(M);
-targets=targets(randidx(1:N),:);
-
 ximages=zeros(N,128^2);
 deltas=zeros(N,128^2);
 dist_ori=zeros(N,1);
@@ -15,7 +11,7 @@ parfor i=1:N
     img=images(i,:);
     img=reshape(img,128,128);
     target=targets(i,:);
-    target=reshape(target,128,128);
+    target=reshape(target,21,21);
     [ximg,delta,dist_ori(i),dist(i)]=histhack3(img,target);    
     ximages(i,:)=ximg(:)';
     deltas(i,:)=delta(:)';
