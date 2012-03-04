ac5396c8718b67fd44b6ffa772b6cbc4cc1db499
Luyi Chen <lychen1109@gmail.com>
2012-03-04 23:55:50 +0800

    changed histhackrun2 for histhack4

--- histhackrun2.m
+++ histhackrun2.m
@@ -1,19 +1,19 @@
 function output=histhackrun2(images,targets,K,T)
-%batch run of histhack3, using parfor
+%batch run of histhack4, using parfor
 
 N=size(images,1);
-ximages=zeros(N,128^2);
-deltas=zeros(N,128^2);
+ximages=zeros(128,128,N);
+deltas=zeros(128,128,N);
 dist_ori=zeros(N,1);
 dist=zeros(N,1);
 parfor i=1:N    
     fprintf('processing image %d\n',i);
-    [ximg,delta,dist_ori(i),dist(i)]=histhack3(images(i,:),targets(i,:),K,T);    
-    ximages(i,:)=ximg(:)';
-    deltas(i,:)=delta(:)';
+    [ximg,delta,dist_ori(i),dist(i)]=histhack4(images(i,:),targets(i,:),K,T);    
+    ximages(:,:,i)=ximg;
+    deltas(:,:,i)=delta;
 end
 
-output.bdctimgs=ximages;
+output.ximages=ximages;
 output.deltas=deltas;
 output.dist_ori=dist_ori;
 output.dist=dist;
