2cd21c7734449b648bdd3e72452e194f1bcdfd12
Luyi Chen <lychen1109@gmail.com>
2012-03-26 19:24:05 +0800

    modify histhackrun2 to fit the new histhack3b

--- histhackrun2.m
+++ histhackrun2.m
@@ -1,20 +1,14 @@
-function output=histhackrun2(images,targets,K,T)
-%batch run of histhack4, using parfor
+function output=histhackrun2(images,targets,beta1,beta2,K,T)
+%batch run of histhack*, using parfor
 
 N=size(images,1);
 ximages=zeros(128,128,N);
-deltas=zeros(128,128,N);
-dist_ori=zeros(N,1);
-dist=zeros(N,1);
+
 parfor i=1:N    
     fprintf('processing image %d\n',i);
-    [ximg,delta,dist_ori(i),dist(i)]=histhack3b(images(i,:),targets(i,:),K,T);    
-    ximages(:,:,i)=ximg;
-    deltas(:,:,i)=delta;
+    [ximages(:,:,i)]=histhack3b(images(i,:),targets(i,:),beta1,beta2,K,T);
 end
 
 output.ximages=ximages;
-output.deltas=deltas;
-output.dist_ori=dist_ori;
-output.dist=dist;
+
 
