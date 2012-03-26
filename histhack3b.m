16f587dc800455da906f1f5b775cacff817a4617
Luyi Chen <lychen1109@gmail.com>
2012-03-26 19:19:04 +0800

    add beta1 and beta2 input

--- histhack3b.m
+++ histhack3b.m
@@ -1,8 +1,9 @@
-function [bdctimg,delta,dist_ori,dist]=histhack3b(img,imgtarget,K,T)
+function [bdctimg,delta,distarray,distarray1,distarray2]=histhack3b(img,imgtarget,beta1,beta2,K,T)
 %change only on bdct domain, but constraint on both space
 %K: dymamic range of coefficients
 %T: threshold of co-occurrence matrix
 
+%fid=fopen('imgproc.log','w');
 %reshape images
 img=reshape(img,128,128);
 imgtarget=reshape(imgtarget,128,128);
@@ -27,8 +28,15 @@ dcmark(1,1)=true;
 dcmark=repmat(dcmark,16,16);
 zeromark=(bdctimg==0);
 
-dist_ori=sampledist(tm,tms,tmtarget,tms_ori);
-dist=dist_ori;
+if nargout>2
+    [dist_ori,dist_ori1,dist_ori2]=sampledist(tm,tms,tmtarget,tms_ori,0);
+    distarray=[];
+    distarray1=[];
+    distarray2=[];
+    distarray=cat(1,distarray,dist_ori);
+    distarray1=cat(1,distarray1,dist_ori1);
+    distarray2=cat(1,distarray2,dist_ori2);
+end
 
 %modify components sorted by potentials
 potential=~(dcmark|zeromark);
@@ -37,7 +45,12 @@ pointsize=length(pointavailable);
 randidx=randperm(pointsize);
 for i=1:pointsize
     [sj,sk]=ind2sub(size(bdctimg),pointavailable(randidx(i)));
-    output=flaggen(bdctimg,tm,tmtarget,img,tms,tms_ori,sj,sk,T,K);
+    if i>round(pointsize/2)
+        beta=beta2;
+    else
+        beta=beta1;
+    end
+    output=flaggen(bdctimg,tm,tmtarget,img,tms,tms_ori,sj,sk,T,K,1,beta);
     if ~output.modified
         continue;
     end
@@ -45,16 +58,25 @@ for i=1:pointsize
     tm=output.tm;
     img=output.newimg;
     tms=output.newtms;
-    dist=output.dist;
+    if nargout>2
+        distarray=cat(1,distarray,output.dist);
+        distarray1=cat(1,distarray1,output.dist1);
+        distarray2=cat(1,distarray2,output.dist2);
+    end
 end
 
 bdctimg=bdctimg.*bdctsign;
 delta=bdctimgori-bdctimg;
+%fclose(fid);
 
-function output=flaggen(bdctimg,tm,tmtarget,img,tms,tms_ori,sj,sk,T,K)
+function output=flaggen(bdctimg,tm,tmtarget,img,tms,tms_ori,sj,sk,T,K,~,beta)
 %calculate the best flag for current pixel
-dist_ori=sampledist(tm,tms,tmtarget,tms_ori);
+%fprintf(fid,'start evaluation at (%d,%d)\n',sj,sk);
+[dist_ori,dist_ori1,dist_ori2]=sampledist(tm,tms,tmtarget,tms_ori,beta);
+%fprintf(fid,'dist_ori=%g,dist_ori1=%g,dist_ori2=%g\n',dist_ori,dist_ori1,dist_ori2);
 output.dist=dist_ori;
+output.dist1=dist_ori1;
+output.dist2=dist_ori2;
 output.modified=false;
 for i=max(-K,-bdctimg(sj,sk)):K
     if i==0
@@ -82,6 +104,7 @@ for i=max(-K,-bdctimg(sj,sk)):K
         %check difference in 8x8
         diffblock=newsblock-img(j0+1:j0+8,k0+1:k0+8);
         changed=find(diffblock);
+        %fprintf(fid,'i=%d,changed pixel=%d\n',i,length(changed));
         
         %calculate newtms
         newtms=tms;
@@ -94,7 +117,8 @@ for i=max(-K,-bdctimg(sj,sk)):K
         end
     end
     tmnew=out.tm;    
-    dist=sampledist(tmnew,newtms,tmtarget,tms_ori);
+    [dist,dist1,dist2]=sampledist(tmnew,newtms,tmtarget,tms_ori,beta);
+    %fprintf(fid,'dist=%g,dist1=%g,dist2=%g\n',dist,dist1,dist2);
     if dist<dist_ori
         output.modified=true;
         dist_ori=dist;
@@ -103,13 +127,19 @@ for i=max(-K,-bdctimg(sj,sk)):K
         output.newimg=newimg;
         output.newtms=newtms;
         output.dist=dist;
+        output.dist1=dist1;
+        output.dist2=dist2;
     end
 end
 
-function dist=sampledist(tm,tms,tmtarget,tms_ori)
+function [dist,dist1,dist2]=sampledist(tm,tms,tmtarget,tms_ori,beta)
 %return normalized distance
 %dist=sum(abs(tm1-tm2).*w(:));
-dist=norm(tm(:)-tmtarget(:))+norm(tms(:)-tms_ori(:));
+dist1=norm(tm(:)-tmtarget(:));
+dist2=norm(tms(:)-tms_ori(:));
+dist=dist1+beta*dist2;
+%dist=norm(tm(:)-tmtarget(:))+norm(tms(:)-tms_ori(:));
+
 
 
 
