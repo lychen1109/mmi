873073785b26747ec83f064345a76196110a7df2
Luyi Chen <lychen1109@gmail.com>
2012-03-29 14:40:03 +0800

    add rangec and rangeg input

--- svm/svmtest.m
+++ svm/svmtest.m
@@ -1,14 +1,15 @@
-function [ac,tp,tn]=svmtest(class,data,ratio)
+function ac=svmtest(class,data,ratio,k,rangec,rangeg)
 %test the svm accuracy with k split
 %ratio is the percentage of training samples
 
-k=20;
-ac=zeros(k,1);
-tp=zeros(k,1);
-tn=zeros(k,1);
+ac=zeros(k,3);
 N=size(class,1);
 ntest=round(N*(1-ratio));
 fprintf('test sample size is %d\n',ntest);
+if nargin<5
+    rangec=0:4;
+    rangeg=-7:2:3;
+end
 
 for i=1:k    
     CVP=cvpartition(class,'holdout',ntest);
@@ -16,18 +17,17 @@ for i=1:k
     grpTrain=class(CVP.training);
     dataTest=data(CVP.test,:);
     grpTest=class(CVP.test);
-    thetas=svmgrid2(grpTrain,dataTrain,0:2:12,-6:2:6);
+    thetas=svmgrid2(grpTrain,dataTrain,rangec,rangeg);
     %[thetas,thresholds]=svmgridtp(grpTrain,dataTrain,0:2:12,-6:2:6);
     [~,~,dvalues]=mysvmfun(grpTrain,dataTrain,grpTest,dataTest,thetas(1,:));
     predict=ones(size(grpTest));
     predict(dvalues<0)=0;
-    ac(i)=sum(predict==grpTest)/length(grpTest);
-    tp(i)=sum(grpTest==0 & predict==grpTest)/sum(grpTest==0);
-    tn(i)=sum(grpTest==1 & predict==grpTest)/sum(grpTest==1);
-    fprintf('accuracy %g tp %g tn %g\n',ac(i),tp(i),tn(i));
+    ac(i,1)=sum(predict==grpTest)/length(grpTest);
+    ac(i,2)=sum(grpTest==0 & predict==grpTest)/sum(grpTest==0);
+    ac(i,3)=sum(grpTest==1 & predict==grpTest)/sum(grpTest==1);
+    fprintf('accuracy %g tp %g tn %g\n',ac(i,:)');
 end
 
-fprintf('final accuracy: %g (%g), tp: %g (%g), tn:%g (%g)\n',mean(ac),std(ac),mean(tp),std(tp),mean(tn),std(tn));
 
 
     
