9fa52406926c574006f2225b55a5685982b3ffc1
Luyi Chen <lychen1109@gmail.com>
2012-04-18 23:55:57 +0800

    modified tmmodcompare to include tmmodrec and tmmodrec2

--- tmmodcompare.m
+++ tmmodcompare.m
@@ -1,27 +1,29 @@
-function tmmodcompare(img)
+function tmmodcompare(img,sj,sk,flag,T)
 %compare performance of tmmod approach
 
 img=reshape(img,128,128);
-tms=tpm1(img,3);
+tms=tpm1(img,T);
 bdctimg=blkproc(img,[8 8],@dct2);
 bdctimg=abs(round(bdctimg));
-tm=tpm1(bdctimg,3);
+tm=tpm1(bdctimg,T);
 
-sj=100;
-sk=101;
 img1=img;
-img1(sj,sk)=img1(sj,sk)+1;
+img1(sj,sk)=img1(sj,sk)+flag;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %normal tpm generation method
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 tic;
-newtms1=tpm1(img1,3);
+newtms1=tpm1(img1,T);
 bdctimg1=blkproc(img1,[8 8],@dct2);
 bdctimg1=abs(round(bdctimg1));
-newtm1=tpm1(bdctimg1,3);
+newtm1=tpm1(bdctimg1,T);
 toc;
 
 %use faster algorithm
 tic;
-out=tmmod2(img,tms,sj,sk,1,3);
+out=tmmod2(img,tms,sj,sk,flag,T);
 newtms2=out.tm;
 j0=floor((sj-1)/8)*8;
 k0=floor((sk-1)/8)*8;
@@ -36,7 +38,7 @@ newtm2=tm;
 newbdctimg=bdctimg;
 for s=1:length(changed)
     [jdelta,kdelta]=ind2sub([8 8],changed(s));
-    outbdct=tmmod2(newbdctimg,newtm2,j0+jdelta,k0+kdelta,diff(jdelta,kdelta),3);
+    outbdct=tmmod2(newbdctimg,newtm2,j0+jdelta,k0+kdelta,diff(jdelta,kdelta),T);
     newbdctimg(j0+jdelta,k0+kdelta)=newblock(jdelta,kdelta);
     newtm2=outbdct.tm;
 end
@@ -44,13 +46,24 @@ toc;
 
 %use tmmod3
 tic;
-[tms3,tm3]=tmmod3(img,bdctimg,tms,tm,sj,sk,1,3);
+[tms3,tm3]=tmmod3(img,bdctimg,tms,tm,sj,sk,flag,T);
+toc;
+
+tic;
+changes=tmmodrec(img,sj,sk,flag,T);
+changef=tmmodrec2(img,bdctimg,sj,sk,flag,T);
+tms4=tms;
+tms4(changes(:,1))=tms4(changes(:,1))+changes(:,2);
+tm4=tm;
+tm4(changef(:,1))=tm4(changef(:,1))+changef(:,2);
 toc;
 
 isequal(newtms1,newtms2)
 isequal(newtm1,newtm2)
 isequal(newtms1,tms3)
 isequal(newtm1,tm3)
+isequal(newtms1,tms4)
+isequal(newtm1,tm4)
 
 
 
