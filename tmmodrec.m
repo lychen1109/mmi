c99d77cdce2bca0e03bc8b39843310e2e1d67e2e
Luyi Chen <lychen1109@gmail.com>
2012-04-18 11:23:46 +0800

    bug fix

--- tmmodrec.m
+++ tmmodrec.m
@@ -25,10 +25,10 @@ if sk+2<129
 end
 
 if sk-1>0 && sk+1<129
-    y1=threshold(bdctimg(sj,sk-1)-bdctimg(sj,sk),T);
-    y2=threshold(bdctimg(sj,sk)-bdctimg(sj,sk+1),T);
-    y3=threshold(bdctimg(sj,sk-1)-bdctimg(sj,sk)-flag,T);
-    y4=threshold(bdctimg(sj,sk)-bdctimg(sj,sk+1)+flag,T);
+    y1=threshold(img(sj,sk-1)-img(sj,sk),T);
+    y2=threshold(img(sj,sk)-img(sj,sk+1),T);
+    y3=threshold(img(sj,sk-1)-img(sj,sk)-flag,T);
+    y4=threshold(img(sj,sk)-img(sj,sk+1)+flag,T);
     change=cat(1,change,[y1+T+1 y2+T+1 -1]);
     change=cat(1,change,[y3+T+1 y4+T+1 1]);
 end
