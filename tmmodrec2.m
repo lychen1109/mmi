b62ac35f3b87129521a3d66a3b24749eaf81f053
Luyi Chen <lychen1109@gmail.com>
2012-04-20 16:15:26 +0800

    add bdctimg modification

--- tmmodrec2.m
+++ tmmodrec2.m
@@ -1,4 +1,4 @@
-function change=tmmodrec2(img,bdctimg,sj,sk,flag,T)
+function [change,diff]=tmmodrec2(img,bdctimg,sj,sk,flag,T)
 %calculate mod effect on bdct domain while image is modified on spatial
 %domain
 
@@ -7,13 +7,14 @@ j0=floor((sj-1)/8)*8;
 k0=floor((sk-1)/8)*8;
 newblock=dct2(img(j0+1:j0+8,k0+1:k0+8));
 newblock=abs(round(newblock));
-diff=newblock-bdctimg(j0+1:j0+8,k0+1:k0+8);
+diff=zeros(size(bdctimg));
+diff(j0+1:j0+8,k0+1:k0+8)=newblock-bdctimg(j0+1:j0+8,k0+1:k0+8);
 bdctchange=find(diff);
 change=zeros(2*T+1,2*T+1);
 for s=1:length(bdctchange)
-    [jdelta,kdelta]=ind2sub([8 8],bdctchange(s));
-    change1=tmmodrec(bdctimg,j0+jdelta,k0+kdelta,diff(jdelta,kdelta),T);
-    bdctimg(j0+jdelta,k0+kdelta)=newblock(jdelta,kdelta);
+    [jdelta,kdelta]=ind2sub(size(bdctimg),bdctchange(s));
+    change1=tmmodrec(bdctimg,jdelta,kdelta,diff(jdelta,kdelta),T);
+    bdctimg(jdelta,kdelta)=bdctimg(jdelta,kdelta)+diff(jdelta,kdelta);
     change(change1(:,1))=change(change1(:,1))+change1(:,2);
 end
 change=nzelements(change);
