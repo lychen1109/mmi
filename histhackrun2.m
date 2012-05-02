b829128d75ae0fc3d48e477dca44a3dc9659641e
Luyi Chen <lychen1109@gmail.com>
2012-05-02 15:14:04 +0800

    use histhack3d in histhackrun2

--- histhackrun2.m
+++ histhackrun2.m
@@ -1,14 +1,21 @@
-function output=histhackrun2(images,targets,beta1,beta2,K,T,B)
+function output=histhackrun2(images,targets,K,T)
 %batch run of histhack*, using parfor
 
 N=size(images,1);
 ximages=zeros(128,128,N);
+distarrays=cell(N,1);
+distarray1s=cell(N,1);
+distarray2s=cell(N,2);
 
 parfor i=1:N    
     fprintf('processing image %d\n',i);
-    [ximages(:,:,i)]=histhack3c(images(i,:),targets(i,:),beta1,beta2,K,T,B);
+    [ximages(:,:,i),distarrays{i},distarray1s{i},distarray2s{i}]=histhack3d(images(i,:),targets(i,:),K,T);
 end
 
-output.ximages=ximages;
+output.ximages=reshape(ximages,128*128,N)';
+output.distarrays=distarrays;
+output.distarray1s=distarray1s;
+output.distarray2s=distarray2s;
+
 
 
