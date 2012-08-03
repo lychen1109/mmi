c0ed0e0036aa9ae02e4051cf8bf018f64b513237
Luyi Chen <lychen1109@gmail.com>
2012-08-03 16:42:34 +0800

    add new output params to bdcttest

--- bdcttest.m
+++ bdcttest.m
@@ -1,4 +1,4 @@
-function bdcttest(img)
+function [tt1,tt2,tt3,tt4,sj,sk,cr1,cr2,cr3]=bdcttest(img)
 %test run time and correctness of new way of calculation of bdct coeff
 
 img=reshape(img,128,128);
@@ -12,6 +12,7 @@ sk=ceil(rand*128);
 %calculate original tm and tms
 bdctimg=blkproc(img,[8 8],@dct2);
 bdctimg=abs(round(bdctimg));
+tm=tpm1(bdctimg,T);
 
 %basic way of calculate new tm
 tic;
@@ -19,8 +20,14 @@ newimg=img;
 newimg(sj,sk)=newimg(sj,sk)+flag;
 newbdctimg=blkproc(newimg,[8 8],@dct2);
 newbdctimg=abs(round(newbdctimg));
+tt1=toc;
+%fprintf('time used in calculate new bdct %g sec\n',tt);
+
+tic;
 newtm=tpm1(newbdctimg,T);
-toc;
+tt2=toc;
+%fprintf('time used in calculate new tpm %g sec\n',tt);
+
 %new way of calculate new tm
 tic;
 j0=floor((sj-1)/8);
@@ -29,18 +36,44 @@ bdctblock=dct2(newimg(j0*8+1:j0*8+8,k0*8+1:k0*8+8));
 bdctblock=abs(round(bdctblock));
 newbdctimg2=bdctimg;
 newbdctimg2(j0*8+1:j0*8+8,k0*8+1:k0*8+8)=bdctblock;
-newtm2=tpm1(newbdctimg2,T);
-toc;
+tt3=toc;
+%fprintf('time used in calculate new bdct %g sec\n',tt);
+
+%calculate new tm
+tic;
+diffblock=newbdctimg2-bdctimg;
+[rows,cols]=find(diffblock);
+newtm2=tm;
+newbdctimg3=bdctimg;
+for i=1:length(rows)
+    out=tmmod2(newbdctimg3,newtm2,rows(i),cols(i),diffblock(rows(i),cols(i)),T);
+    newtm2=out.tm;
+    newbdctimg3(rows(i),cols(i))=newbdctimg3(rows(i),cols(i))+diffblock(rows(i),cols(i));
+end
+tt4=toc;
+%fprintf('time used in calculate new tpm %g sec\n',tt);
 
 if isequal(newbdctimg,newbdctimg2)
-    fprintf('bdct is same\n');
+    %fprintf('newbdctimg and newbdctimg2 are same\n');
+    cr1=true;
 else
-    fprintf('bdct is differenct\n');
+    %fprintf('newbdctimg and newbdctimg2 are differenct\n');
+    cr1=false;
+end
+
+if isequal(newbdctimg,newbdctimg3)
+    %fprintf('newbdctimg and newbdctimg3 are same\n');
+    cr2=true;
+else
+    %fprintf('newbdctimg and newbdctimg3 are different\n');
+    cr2=false;
 end
 
 if isequal(newtm,newtm2)
-    fprintf('tm is same\n');
+    %fprintf('tm is same\n');
+    cr3=true;
 else
-    fprintf('tm is different\n');
+    %fprintf('tm is different\n');
+    cr3=false;
 end
 
