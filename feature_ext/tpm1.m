7d9f5ff1235b09751ff7ce22ba4c8dbf59742f42
Luyi Chen <lychen1109@gmail.com>
2012-11-17 14:37:24 +0800

    change to use the last row in calculation

--- feature_ext/tpm1.m
+++ feature_ext/tpm1.m
@@ -4,7 +4,7 @@ function D=tpm1(img,T)
 [N,M]=size(img);
 % img=blkproc(img,[8 8],@dct2);
 % img=abs(round(img));
-diffimg=img(1:N-1,1:M-1)-img(1:N-1,2:M);
+diffimg=img(1:N,1:M-1)-img(1:N,2:M);
 %diffimg=round(diffimg);
 
 D=tpmf(diffimg,T);
